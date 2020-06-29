cb0111f39cb76ee12993d9d0415409161a4e86a8
Nitish Singla <nitish.singla@hmcts.net>
2020-06-29 14:25:33 +0100

    Cleanup the suppressions.xml

--- src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/controller/WelcomeControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/controller/WelcomeControllerIntegrationTest.java
@@ -34,9 +34,9 @@ public class WelcomeControllerIntegrationTest extends BaseTest {
     private transient String url;
 
     private static final MediaType JSON_CONTENT_TYPE = new MediaType(
-        MediaType.APPLICATION_JSON.getType(),
-        MediaType.APPLICATION_JSON.getSubtype(),
-        Charset.forName("utf8")
+            MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8")
     );
 
     @Autowired
@@ -52,31 +52,31 @@ public class WelcomeControllerIntegrationTest extends BaseTest {
     public void welComeAPITest() throws Exception {
         logger.info(" WelcomeControllerIntegrationTest : Inside  Welcome API Test method...{}", url);
         final MvcResult result = mockMvc.perform(get(url).contentType(JSON_CONTENT_TYPE))
-                                        //.andExpect(status().is(200))
-                                        .andReturn();
+                //.andExpect(status().is(200))
+                .andReturn();
         assertEquals(
-            "Welcome service status", 200, 200);
+                "Welcome service status", 200, 200);
     }
 
     @Test
-    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:sql/insert_role_assignment_request.sql"})
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+            scripts = {"classpath:sql/insert_role_assignment_request.sql"})
     public void shoudGetRecordCountFromRequestTable() throws Exception {
         final int count = template.queryForObject(COUNT_RECORDS, Integer.class);
         logger.info(" Total number of records fetched from Organisation Role Mapping request table...{}", count);
         assertEquals(
-            "role_assignment_request record count ", 5, count);
+                "role_assignment_request record count ", 5, count);
     }
 
     @Test
-    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:sql/insert_role_assignment_request.sql"})
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+            scripts = {"classpath:sql/insert_role_assignment_request.sql"})
     public void shoudGetRequestStatusFromRequestTable() throws Exception {
-        final Object[] parameters = new Object[]{
-            REQUEST_ID
-        };
+        final Object[] parameters = new Object[]{REQUEST_ID};
         String status = template.queryForObject(GET_STATUS, parameters, String.class);
         logger.info(" Organisation Role Mapping request status is...{}", status);
         assertEquals(
-            "Organisation Role Mapping request status", "APPROVED", status);
+                "Organisation Role Mapping request status", "APPROVED", status);
     }
 
 }
