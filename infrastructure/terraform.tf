4e1ea20d176191dafef1ac801537e2d797fc73dd
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2020-10-02 09:44:27 +0100

    TF v0.13.3 upgrade changes (#13)
    * TF v0.13.3 upgrae changes
    
    * Upgrade grade version to 6.6
    
    * Fixing the dependency check issues.
    
    * Fixing the dependency check issues.
    
    * Fixing the dependency check issues.
    
    * Fixing the dependency check issues.
    
    * Bumping chart version
    
    * Downgrading befta fw.
    
    Co-authored-by: Nitish Singla <nitish.singla@hmcts.net>
    Co-authored-by: hmcts-jenkins-am <62423932+hmcts-jenkins-am[bot]@users.noreply.github.com>

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,14 +1,14 @@
+provider "azurerm" {
+  features {}
+}
+
 terraform {
   backend "azurerm" {}
-
 
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "~> 2.25"
-    }
-    random = {
-      source = "hashicorp/random"
     }
   }
 }
