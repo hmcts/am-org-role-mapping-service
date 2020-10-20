7b35048c555dd43e56a815b01c65f0644b58b76b
Nitish Singla <nitish.singla@hmcts.net>
2020-10-20 10:55:38 +0100

    Am-1067: Adding Launch Darkly secrets.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/config/LaunchDarklyConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/config/LaunchDarklyConfiguration.java
@@ -1,15 +1,18 @@
 package uk.gov.hmcts.reform.orgrolemapping.config;
 
 import com.launchdarkly.sdk.server.LDClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class LaunchDarklyConfiguration {
 
     @Bean
     public LDClient ldClient(@Value("${launchdarkly.sdk.key}") String sdkKey) {
+        log.error("sdkKey is :" + sdkKey);
         return new LDClient(sdkKey);
     }
 
