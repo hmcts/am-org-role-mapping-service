9e1389363c57501fe4172fa1639467d74e845661
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-10-20 11:19:41 +0100

    oidc support for orm

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/config/SecurityConfiguration.java
@@ -1,68 +1,128 @@
 package uk.gov.hmcts.reform.orgrolemapping.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
 import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
 import uk.gov.hmcts.reform.auth.checker.core.service.Service;
 import uk.gov.hmcts.reform.auth.checker.core.user.User;
 import uk.gov.hmcts.reform.auth.checker.spring.serviceanduser.AuthCheckerServiceAndUserFilter;
+import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
+import uk.gov.hmcts.reform.orgrolemapping.oidc.JwtGrantedAuthoritiesConverter;
+
+import javax.inject.Inject;
+import java.util.List;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
+
 @Configuration
+@ConfigurationProperties(prefix = "security")
 @EnableWebSecurity
+@Slf4j
+@SuppressWarnings("unchecked")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final AuthCheckerServiceAndUserFilter authCheckerFilter;
+    @Value("${spring.security.oauth2.client.provider.oidc.issuer-uri}")
+    private String issuerUri;
 
-    @Autowired
-    public SecurityConfiguration(final RequestAuthorizer<User> userRequestAuthorizer,
-                                 final RequestAuthorizer<Service> serviceRequestAuthorizer,
-                                 final AuthenticationManager authenticationManager) {
-        super();
+    @Order(1)
+    private final ServiceAuthFilter serviceAuthFilter;
 
-        this.authCheckerFilter = new AuthCheckerServiceAndUserFilter(serviceRequestAuthorizer, userRequestAuthorizer);
+    @Order(2)
+    private final SecurityEndpointFilter securityEndpointFilter;
 
-        this.authCheckerFilter.setAuthenticationManager(authenticationManager);
+    List<String> anonymousPaths;
+    private final JwtAuthenticationConverter jwtAuthenticationConverter;
+
+    public List<String> getAnonymousPaths() {
+        return anonymousPaths;
+    }
+
+    public void setAnonymousPaths(List<String> anonymousPaths) {
+        this.anonymousPaths = anonymousPaths;
+    }
+
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-ui.html",
+            "/webjars/springfox-swagger-ui/**",
+            "/swagger-resources/**",
+            "/v2/**",
+            "/status/health",
+            "/welcome",
+            "/health/**",
+            "/health/liveness",
+            "/loggers/**",
+            "/"
+    };
+
+
+    @Inject
+    public SecurityConfiguration(final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter,
+                                 final ServiceAuthFilter serviceAuthFilter,
+                                 SecurityEndpointFilter securityEndpointFilter) {
+
+        this.serviceAuthFilter = serviceAuthFilter;
+        this.securityEndpointFilter = securityEndpointFilter;
+        jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+
     }
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/swagger-ui.html",
-                "/webjars/springfox-swagger-ui/**",
-                "/swagger-resources/**",
-                "/v2/**",
-                "/actuator/**",
-                "/health",
-                "/welcome",
-                "/health/**",
-                "/status/health",
-                "/loggers/**",
-                "/");
+        web.ignoring().antMatchers(AUTH_WHITELIST);
     }
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        // Don't erase user credentials as this is needed for the user profile
-        final ProviderManager authenticationManager = (ProviderManager) authenticationManager();
-        authenticationManager.setEraseCredentialsAfterAuthentication(false);
-        authCheckerFilter.setAuthenticationManager(authenticationManager());
-
         http
-                .addFilter(authCheckerFilter)
+                .addFilterBefore(serviceAuthFilter, BearerTokenAuthenticationFilter.class)
+                .addFilterAfter(securityEndpointFilter, OAuth2AuthorizationRequestRedirectFilter.class)
                 .sessionManagement().sessionCreationPolicy(STATELESS).and()
                 .csrf().disable()
                 .formLogin().disable()
                 .logout().disable()
                 .authorizeRequests()
+                .antMatchers("/error").permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter)
+                .and()
+                .and()
+                .oauth2Client();
+    }
 
+    @Bean
+    JwtDecoder jwtDecoder() {
+
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuerUri);
+        OAuth2TokenValidator<Jwt> withTimestamp = new JwtTimestampValidator();
+        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp);
+        jwtDecoder.setJwtValidator(validator);
+        return jwtDecoder;
     }
 }
---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/oidc/IdamRepository.java
@@ -0,0 +1,100 @@
+package uk.gov.hmcts.reform.orgrolemapping.oidc;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.idam.client.IdamApi;
+import uk.gov.hmcts.reform.idam.client.OAuth2Configuration;
+import uk.gov.hmcts.reform.idam.client.models.TokenRequest;
+import uk.gov.hmcts.reform.idam.client.models.TokenResponse;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+
+import java.util.List;
+
+import static org.springframework.http.HttpMethod.GET;
+import static uk.gov.hmcts.reform.orgrolemapping.apihelper.Constants.BEARER;
+
+@Component
+@Slf4j
+public class IdamRepository {
+
+    private IdamApi idamApi;
+    private OIdcAdminConfiguration oidcAdminConfiguration;
+    private OAuth2Configuration oauth2Configuration;
+    private RestTemplate restTemplate;
+    @Value("${idam.api.url}")
+    protected String idamUrl;
+
+    @Autowired
+    public IdamRepository(IdamApi idamApi,
+                          OIdcAdminConfiguration oidcAdminConfiguration,
+                          OAuth2Configuration oauth2Configuration,
+                          RestTemplate restTemplate) {
+        this.idamApi = idamApi;
+        this.oidcAdminConfiguration = oidcAdminConfiguration;
+        this.oauth2Configuration = oauth2Configuration;
+        this.restTemplate = restTemplate;
+    }
+
+    public UserInfo getUserInfo(String jwtToken) {
+        return idamApi.retrieveUserInfo(BEARER + jwtToken);
+    }
+
+    public UserDetails getUserByUserId(String jwtToken, String userId) {
+        return idamApi.getUserByUserId(BEARER + jwtToken, userId);
+    }
+
+    public ResponseEntity<List<Object>> searchUserByUserId(String jwtToken, String userId) {
+        try {
+            String url = String.format("%s/api/v1/users?query=%s", idamUrl, userId);
+            ResponseEntity<List<Object>> response = restTemplate.exchange(
+                    url,
+                    GET,
+                    new HttpEntity<>(getHttpHeaders(jwtToken)),
+                    new ParameterizedTypeReference<List<Object>>() {
+                    }
+            );
+            if (HttpStatus.OK.equals(response.getStatusCode())) {
+                return response;
+            }
+        } catch (Exception exception) {
+            log.info(exception.getMessage());
+            throw exception;
+        }
+        return null;
+    }
+
+    private static HttpHeaders getHttpHeaders(String jwtToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken);
+        return headers;
+    }
+
+    @Cacheable(value = "token")
+    public String getManageUserToken() {
+        TokenRequest tokenRequest = new TokenRequest(
+                oauth2Configuration.getClientId(),
+                oauth2Configuration.getClientSecret(),
+                "password",
+                "",
+                oidcAdminConfiguration.getUserId(),
+                oidcAdminConfiguration.getSecret(),
+                oidcAdminConfiguration.getScope(),
+                "4",
+                ""
+        );
+        TokenResponse tokenResponse = idamApi.generateOpenIdToken(tokenRequest);
+        return tokenResponse.accessToken;
+    }
+
+}
