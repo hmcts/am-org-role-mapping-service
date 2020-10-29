2c10167d90ea653a4d91cbbcde652a1a77014d46
ConorH994 <conor.halpin@hmcts.net>
2020-10-29 16:18:35 +0000

    sonar smells

--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/RoleAssignmentServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/RoleAssignmentServiceTest.java
@@ -12,14 +12,14 @@ import uk.gov.hmcts.reform.orgrolemapping.feignclients.RASFeignClient;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
-public class RoleAssignmentServiceTest {
+class RoleAssignmentServiceTest {
 
     RASFeignClient rasFeignClient = mock(RASFeignClient.class);
 
     RoleAssignmentService sut = new RoleAssignmentService(rasFeignClient);
 
     @Test
-    public void testRASFeignClient() {
+    void testRASFeignClient() {
         Mockito.when(rasFeignClient.createRoleAssignment(ArgumentMatchers.any(AssignmentRequest.class)))
                 .thenReturn(ResponseEntity.status(HttpStatus.CREATED).body("RoleAssignment"));
         ResponseEntity<Object> responseEntity = sut.createRoleAssignment(AssignmentRequest.builder().build());
