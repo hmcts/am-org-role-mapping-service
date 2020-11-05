bc2bf3f159ea60410f5d52d7915baf28b4dab4ac
ConorH994 <conor.halpin@hmcts.net>
2020-11-05 09:07:34 +0000

    began implementing smoke test for create endpoint

--- src/smokeTest/java/uk/gov/hmcts/reform/orgrolemapping/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/orgrolemapping/SmokeTest.java
@@ -10,11 +10,18 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.jupiter.api.Tag;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.reform.orgrolemapping.oidc.IdamRepository;
+import uk.gov.hmcts.reform.orgrolemapping.util.SecurityUtils;
+
+import static uk.gov.hmcts.reform.orgrolemapping.apihelper.Constants.AUTHORIZATION;
+import static uk.gov.hmcts.reform.orgrolemapping.apihelper.Constants.BEARER;
+import static uk.gov.hmcts.reform.orgrolemapping.apihelper.Constants.SERVICE_AUTHORIZATION;
 
 @RunWith(SpringIntegrationSerenityRunner.class)
 @NoArgsConstructor
@@ -31,6 +38,12 @@ public class SmokeTest {
     private String sdkKey;
 
     UserTokenProviderConfig config;
+
+    @Autowired
+    SecurityUtils securityUtils;
+
+    @Autowired
+    private IdamRepository idamRepository;
 
     @Rule
     public FeatureFlagToggleEvaluator featureFlagToggleEvaluator = new FeatureFlagToggleEvaluator(this);
@@ -58,6 +71,29 @@ public class SmokeTest {
         response.then().assertThat().statusCode(HttpStatus.OK.value());
     }
 
+    @Tag("smoke")
+    @Test
+    @FeatureFlagToggle("orm-create-flag")
+    public void should_receive_response_for_create_org_mapping() {
+
+        String targetInstance = config.getOrgRoleMappingUrl() + "/am/role-mapping/staff/users";
+        RestAssured.useRelaxedHTTPSValidation();
+
+        String requestBody = "21334a2b-79ce-44eb-9168-2d49a744be9c";
+
+        Response response = SerenityRest
+                .given()
+                .relaxedHTTPSValidation()
+                .header("Content-Type", "application/json")
+                .header(SERVICE_AUTHORIZATION, BEARER + securityUtils.getServiceAuthorizationHeader())
+                .header(AUTHORIZATION, BEARER + idamRepository.getUserToken())
+                .body(requestBody)
+                .when()
+                .post(targetInstance)
+                .andReturn();
+        response.then().assertThat().statusCode(HttpStatus.OK.value());
+    }
+
     public String getEnvironment() {
         return environment;
     }
@@ -70,3 +106,4 @@ public class SmokeTest {
         return sdkKey;
     }
 }
+
