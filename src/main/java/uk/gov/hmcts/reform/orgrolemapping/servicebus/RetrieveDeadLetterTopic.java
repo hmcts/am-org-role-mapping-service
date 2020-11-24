797de94d65ae080a090fccfa99b566dae242cd5f
Nitin <nitin.sahu@hmcts.net>
2020-11-24 12:15:20 +0000

    AM-1107: disabled RAS call..

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/RetrieveDeadLetterTopic.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/RetrieveDeadLetterTopic.java
@@ -5,25 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.gson.Gson;
 import com.microsoft.azure.servicebus.ClientFactory;
-import com.microsoft.azure.servicebus.ExceptionPhase;
 import com.microsoft.azure.servicebus.IMessage;
-import com.microsoft.azure.servicebus.IMessageHandler;
 import com.microsoft.azure.servicebus.IMessageReceiver;
-import com.microsoft.azure.servicebus.MessageHandlerOptions;
 import com.microsoft.azure.servicebus.ReceiveMode;
-import com.microsoft.azure.servicebus.SubscriptionClient;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.time.Duration;
 import java.time.Instant;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Slf4j
 public class RetrieveDeadLetterTopic {
@@ -33,58 +22,6 @@ public class RetrieveDeadLetterTopic {
     public static void main(String[] args) throws Exception {
         deleteMessagesInEnvelopesDlq();
         log.info("clients registered.....");
-    }
-
-    static void registerMessageHandlerOnClient(SubscriptionClient receiveClient) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        log.info("registerMessageHandlerOnClient.....");
-        IMessageHandler messageHandler = new IMessageHandler() {
-            // callback invoked when the message handler loop has obtained a message
-            @SneakyThrows
-            public CompletableFuture<Void> onMessageAsync(IMessage message) {
-                log.info("onMessageAsync.....{}", message);
-                List<byte[]> body = message.getMessageBody().getBinaryData();
-                log.info("body.....{}", body);
-                Integer users = null;
-                try {
-                    users = mapper.readValue(body.get(0), Integer.class);
-                } catch (IOException e) {
-                    try {
-                        receiveClient.abandon(message.getLockToken());
-                    } catch (InterruptedException | ServiceBusException ex) {
-                        ex.printStackTrace();
-                    }
-                    throw e;
-                }
-                System.out.printf(
-                        "\n\t\t\t\t%s Message received: \n\t\t\t\t\t\tMessageId = %s,
-                        \n\t\t\t\t\t\tSequenceNumber = %s, \n\t\t\t\t\t\tEnqueuedTimeUtc = %s," +
-                                "\n\t\t\t\t\t\tExpiresAtUtc = %s, \n\t\t\t\t\t\tContentType = \"%s\",
-                                \n\t\t\t\t\t\tContent: [ User Id = %s]\n",
-                        receiveClient.getEntityPath(),
-                        message.getMessageId(),
-                        message.getSequenceNumber(),
-                        message.getEnqueuedTimeUtc(),
-                        message.getExpiresAtUtc(),
-                        message.getContentType(),
-                        "",
-                        "");
-
-                System.out.printf("Message consumed successfully..... ");
-                return receiveClient.completeAsync(message.getLockToken());
-            }
-
-            public void notifyException(Throwable throwable, ExceptionPhase exceptionPhase) {
-                System.out.printf(exceptionPhase + "-" + throwable.getMessage());
-            }
-        };
-
-        ExecutorService executorService = Executors.newFixedThreadPool(1);
-        receiveClient.registerMessageHandler(
-                messageHandler,new MessageHandlerOptions(1,
-                        false, Duration.ofMinutes(1), Duration.ofMinutes(1)), executorService);
-
     }
 
     //@Scheduled(cron = "${scheduling.task.delete-envelopes-dlq-messages.cron}")
@@ -106,7 +43,7 @@ public class RetrieveDeadLetterTopic {
 
                 //DO not log user ID's
                 log.info("Message is : " + mapper.readValue(message.getMessageBody().getBinaryData().get(0),
-                String.class));
+                        String.class));
                 log.info("Message logging complete");
                 messageReceiver.complete(message.getLockToken());
                 completedCount++;
@@ -145,13 +82,14 @@ public class RetrieveDeadLetterTopic {
 
 
     public static IMessageReceiver getMessageReceiver() throws InterruptedException, ServiceBusException {
-        String connectionString = "Endpoint=sb://rd-servicebus-sandbox.servicebus.windows.net/;
-        SharedAccessKeyName=SendAndListenSharedAccessKey;SharedAccessKey=97E6uvE6xHcqHAVlxufN1PH75tMHoZUe78FhsCbLLLQ=";
+        String connectionString = "Endpoint=sb://rd-servicebus-sandbox.servicebus.windows.net/;" +
+                "SharedAccessKeyName=SendAndListenSharedAccessKey;" +
+                "SharedAccessKey=97E6uvE6xHcqHAVlxufN1PH75tMHoZUe78FhsCbLLLQ=";
 
         try {
             return ClientFactory.createMessageReceiverFromConnectionStringBuilder(new ConnectionStringBuilder
-                    (connectionString, "rd-caseworker-topic-sandbox/subscriptions/temporary/$deadletterqueue"),
-                    ReceiveMode.PEEKLOCK);
+                    (connectionString, "rd-caseworker-topic-sandbox/subscriptions/temporary/$deadletterqueue")
+                    , ReceiveMode.PEEKLOCK);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             log.info("Interrupted");
@@ -162,5 +100,4 @@ public class RetrieveDeadLetterTopic {
         }
     }
 
-}
-*/
+}*/
