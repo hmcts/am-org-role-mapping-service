f316b3125c94d0bb49b07a9cc448b9c9d0df3675
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-12-14 17:46:29 +0000

    AM-1131: remove the key.

--- bin/sendMessage.sh
+++ bin/sendMessage.sh
@@ -4,7 +4,7 @@ file_name=${1:-"example1.json"}
 environment=${2:-"aat"}
 file="../src/main/resources/${file_name}"
 echo "using file: ${file_name} environment:${environment}"
-shared_access_signature="SharedAccessSignature sr=sb%3A%2F%2Frd-servicebus-aat.servicebus.windows.net&sig=okr8Zwd2SSKXSbeTB1coc3rMlP6PafVndathZwCKbZI%3D&se=1608230338&skn=SendAndListenSharedAccessKey"
+shared_access_signature="SharedAccessSignature sr=sb%test"
 
 
 curl -v \
