570ddb578bd77d3f5610bc0bb117bd21695ccdc3
Nitish Singla <nitish.singla@hmcts.net>
2021-01-12 08:44:09 +0000

    AM-1143: Fixing the integration test error.

--- src/functionalTest/java/uk/gov/hmcts/reform/orgrolemapping/befta/OrgRoleMappingAmTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/orgrolemapping/befta/OrgRoleMappingAmTestAutomationAdapter.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.orgrolemapping.befta;
 
-import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
@@ -16,7 +15,11 @@ public class OrgRoleMappingAmTestAutomationAdapter extends DefaultTestAutomation
 
     public static final String EMAIL_TEMPLATE = "CWR-func-test-user-%s@cwrfunctestuser.com";
 
-    @SneakyThrows
+    @Override
+    public void doLoadTestData() {
+
+    }
+
     @Override
     public Object calculateCustomValue(BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
         //the docAMUrl is is referring the self link in PR
@@ -27,7 +30,11 @@ public class OrgRoleMappingAmTestAutomationAdapter extends DefaultTestAutomation
                 return String.format(EMAIL_TEMPLATE, randomAlphanumeric(10)).toLowerCase();
             case ("waitForTime"):
                 logger.info("Sleeping for 10 seconds");
-                Thread.sleep(10000);
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException exception) {
+                    logger.info(exception.getMessage());
+                }
                 logger.info("The nap is complete.");
                 return null;
             default:
