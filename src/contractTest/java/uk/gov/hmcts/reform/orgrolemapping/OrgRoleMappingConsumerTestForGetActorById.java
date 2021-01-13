5a9efda98b245b11e07bc4ba4aa888e7539711c0
ConorH994 <conor.halpin@hmcts.net>
2021-01-13 14:27:37 +0000

    checkstyle

--- src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/OrgRoleMappingConsumerTestForGetActorById.java
+++ src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/OrgRoleMappingConsumerTestForGetActorById.java
@@ -86,18 +86,18 @@ public class OrgRoleMappingConsumerTestForGetActorById {
     private DslPart createResponse() {
         return newJsonBody(o -> o
                 .minArrayLike("roleAssignmentResponse", 1, 1,
-                        roleAssignmentResponse -> roleAssignmentResponse
-                                .stringType("id", "14a21569-eb80-4681-b62c-6ae2ed069e6f")
-                                .stringValue("actorIdType", "IDAM")
-                                .stringValue("actorId", ACTOR_ID)
-                                .stringValue("roleType", "ORGANISATION")
-                                .stringValue("roleName", "senior-tribunal-caseworker")
-                                .stringValue("classification", "PRIVATE")
-                                .stringValue("grantType", "STANDARD")
-                                .stringValue("roleCategory", "STAFF")
-                                .booleanValue("readOnly", false)
-                                .object("attributes", attribute -> attribute
-                                        .stringType("jurisdiction", "IA"))
+                    roleAssignmentResponse -> roleAssignmentResponse
+                        .stringType("id", "14a21569-eb80-4681-b62c-6ae2ed069e6f")
+                        .stringValue("actorIdType", "IDAM")
+                        .stringValue("actorId", ACTOR_ID)
+                        .stringValue("roleType", "ORGANISATION")
+                        .stringValue("roleName", "senior-tribunal-caseworker")
+                        .stringValue("classification", "PRIVATE")
+                        .stringValue("grantType", "STANDARD")
+                        .stringValue("roleCategory", "STAFF")
+                        .booleanValue("readOnly", false)
+                        .object("attributes", attribute -> attribute
+                                .stringType("jurisdiction", "IA"))
                 )).build();
     }
 
