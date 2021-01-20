f19e8996053454f194f5707de2e9a8d4214649f2
Nitish Singla <nitish.singla@hmcts.net>
2021-01-20 09:27:43 +0000

    Fixing the code smells.

--- src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/controller/BaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/controller/BaseTest.java
@@ -11,8 +11,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseTest {
-    private BaseTest() {
-    }
     protected static final ObjectMapper mapper = new ObjectMapper();
 
     @BeforeClass
