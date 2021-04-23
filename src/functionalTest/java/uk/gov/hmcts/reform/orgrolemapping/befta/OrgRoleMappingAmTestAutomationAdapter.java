46be903377b1636f8becc4ed12f9fbeb28ed7af8
ConorH994 <conor.halpin@hmcts.net>
2021-04-23 14:43:58 +0100

    reduced func test wait time to 20s

--- src/functionalTest/java/uk/gov/hmcts/reform/orgrolemapping/befta/OrgRoleMappingAmTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/orgrolemapping/befta/OrgRoleMappingAmTestAutomationAdapter.java
@@ -30,7 +30,7 @@ public class OrgRoleMappingAmTestAutomationAdapter extends DefaultTestAutomation
             case ("generateEmailId"):
                 return String.format(EMAIL_TEMPLATE, randomAlphanumeric(10)).toLowerCase();
             case ("waitForTime"):
-                logger.info("Sleeping for 30 seconds");
+                logger.info("Sleeping for 20 seconds");
                 try {
                     Thread.sleep(20000);
                 } catch (InterruptedException exception) {
