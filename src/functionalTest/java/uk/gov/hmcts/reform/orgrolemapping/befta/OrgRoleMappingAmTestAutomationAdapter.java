03619547b48c7d29d59e481d48dac95f44acd807
dharmendra8186 <dharmendra.singh@hmcts.net>
2021-06-03 17:17:39 +0100

    AM-1408: Change the page size configuration.

--- src/functionalTest/java/uk/gov/hmcts/reform/orgrolemapping/befta/OrgRoleMappingAmTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/orgrolemapping/befta/OrgRoleMappingAmTestAutomationAdapter.java
@@ -25,9 +25,9 @@ public class OrgRoleMappingAmTestAutomationAdapter extends DefaultTestAutomation
             case ("generateEmailId"):
                 return String.format(EMAIL_TEMPLATE, randomAlphanumeric(10)).toLowerCase();
             case ("waitForTime"):
-                logger.info("Sleeping for 45 seconds");
+                logger.info("Sleeping for 20 seconds");
                 try {
-                    Thread.sleep(45000);
+                    Thread.sleep(20000);
                 } catch (InterruptedException exception) {
                     logger.info(exception.getMessage());
                 }
