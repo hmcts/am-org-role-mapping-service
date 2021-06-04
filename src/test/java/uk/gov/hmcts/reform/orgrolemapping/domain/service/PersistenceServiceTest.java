f9c46af6a4af79892654a66ed2a92e3ed6217e52
Nitin <nitin.sahu@hmcts.net>
2021-06-04 16:40:21 +0100

    Added JUnit tests for conditional coverage

--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/PersistenceServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/PersistenceServiceTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.orgrolemapping.domain.service;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
+import uk.gov.hmcts.reform.orgrolemapping.data.FlagConfig;
 import uk.gov.hmcts.reform.orgrolemapping.data.FlagConfigRepository;
 import uk.gov.hmcts.reform.orgrolemapping.data.RefreshJobEntity;
 import uk.gov.hmcts.reform.orgrolemapping.data.RefreshJobsRepository;
@@ -11,8 +12,12 @@ import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class PersistenceServiceTest {
 
@@ -45,5 +50,52 @@ class PersistenceServiceTest {
         Mockito.when(refreshJobsRepository.save(refreshEntity))
                 .thenReturn(refreshEntity);
         sut.persistRefreshJob(refreshEntity);
+    }
+
+    @Test
+    void getFlagStatus() {
+        String flagName = "iac_1_0";
+        String env = "pr";
+        when(flagConfigRepository.findByFlagNameAndEnv(flagName, env)).thenReturn(getFlagConfig(Boolean.TRUE));
+        Boolean response = sut.getStatusByParam(flagName, env);
+        assertTrue(response);
+
+    }
+
+    @Test
+    void getFlagWhenStatusIsFalse() {
+        String flagName = "iac_1_0";
+        String env = "pr";
+        when(flagConfigRepository.findByFlagNameAndEnv(flagName, env)).thenReturn(getFlagConfig(Boolean.FALSE));
+        Boolean response = sut.getStatusByParam(flagName, env);
+        assertFalse(response);
+
+    }
+
+    @Test
+    void getFlagStatusWhenEnvIsEmpty() {
+        String flagName = "iac_1_0";
+        String env = "pr";
+        when(flagConfigRepository.findByFlagNameAndEnv(any(), any())).thenReturn(getFlagConfig(Boolean.TRUE));
+        Boolean response = sut.getStatusByParam(flagName, "");
+        assertTrue(response);
+    }
+
+    @Test
+    void persistFlagConfig() {
+        FlagConfig flagConfig = getFlagConfig(Boolean.TRUE);
+        when(flagConfigRepository.save(flagConfig)).thenReturn(flagConfig);
+        FlagConfig flagConfigEntity = sut.persistFlagConfig(flagConfig);
+        assertNotNull(flagConfigEntity);
+
+    }
+
+    private FlagConfig getFlagConfig(Boolean status) {
+        return FlagConfig.builder()
+                .env("pr")
+                .flagName("iac_1_0")
+                .serviceName("iac")
+                .status(status)
+                .build();
     }
 }
