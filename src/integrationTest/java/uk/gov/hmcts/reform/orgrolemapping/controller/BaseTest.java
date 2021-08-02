feb68dbfe642c48a681cb0ee306520db2465e33a
dharmendra8186 <dharmendra.singh@hmcts.net>
2021-08-02 17:25:02 +0100

    AM-1536: Fixed checkstyle issue.

--- src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/controller/BaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/controller/BaseTest.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.orgrolemapping.servicebus.TopicPublisher;
+
 
 import javax.annotation.PreDestroy;
 import javax.sql.DataSource;
@@ -32,8 +32,7 @@ public abstract class BaseTest {
     @MockBean
     ServiceBusSenderClient serviceBusSenderClient;
 
-    @MockBean
-    TopicPublisher topicPublisher;
+
 
     @BeforeClass
     public static void init() {
