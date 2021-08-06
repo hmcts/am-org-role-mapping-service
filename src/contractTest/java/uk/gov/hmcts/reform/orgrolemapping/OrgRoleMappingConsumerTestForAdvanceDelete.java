dec7a7a4378a36a6052f0529e73d7753cc2d2077
dharmendra8186 <dharmendra.singh@hmcts.net>
2021-08-06 15:04:25 +0100

    AM-1536: pact test compilation issue.

--- src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/OrgRoleMappingConsumerTestForAdvanceDelete.java
+++ src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/OrgRoleMappingConsumerTestForAdvanceDelete.java
@@ -23,9 +23,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.reform.orgrolemapping.servicebus.MessagingConfiguration;
-import uk.gov.hmcts.reform.orgrolemapping.servicebus.TopicConsumer;
-import uk.gov.hmcts.reform.orgrolemapping.servicebus.TopicPublisher;
+import uk.gov.hmcts.reform.orgrolemapping.servicebus.CRDMessagingConfiguration;
+import uk.gov.hmcts.reform.orgrolemapping.servicebus.CRDTopicConsumer;
+import uk.gov.hmcts.reform.orgrolemapping.servicebus.CRDTopicPublisher;
+import uk.gov.hmcts.reform.orgrolemapping.servicebus.JRDMessagingConfiguration;
+import uk.gov.hmcts.reform.orgrolemapping.servicebus.JRDTopicConsumer;
+import uk.gov.hmcts.reform.orgrolemapping.servicebus.JRDTopicPublisher;
 
 @ExtendWith(PactConsumerTestExt.class)
 @ExtendWith(SpringExtension.class)
@@ -44,13 +47,20 @@ public class OrgRoleMappingConsumerTestForAdvanceDelete {
             + ".post-assignments-delete-request+json;charset=UTF-8;version=1.0";
 
     @MockBean
-    TopicConsumer topicConsumer;
+    JRDTopicConsumer crdConsumer;
+    @MockBean
+    CRDTopicConsumer jrdConsumer;
 
     @MockBean
-    TopicPublisher topicPublisher;
+    JRDTopicPublisher jrdPublisher;
+    @MockBean
+    CRDTopicPublisher crdPublisher;
 
     @MockBean
-    MessagingConfiguration messagingConfiguration;
+    JRDMessagingConfiguration jrdMessagingConfiguration;
+
+    @MockBean
+    CRDMessagingConfiguration crdMessagingConfiguration;
 
     @MockBean
     ServiceBusSenderClient serviceBusSenderClient;
