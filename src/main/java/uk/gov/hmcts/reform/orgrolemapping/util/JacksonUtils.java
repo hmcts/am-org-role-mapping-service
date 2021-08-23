9917adf465f386839f3d99bb7dfd34f6cf88e709
dharmendra8186 <dharmendra.singh@hmcts.net>
2021-08-23 11:22:00 +0100

    AM-1536: remove unused import.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/util/JacksonUtils.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/util/JacksonUtils.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.orgrolemapping.util;
 
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
