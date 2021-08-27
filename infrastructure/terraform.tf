a8ff723ce986aa451c1f0d3b218306f512e85c1d
Nitish Singla <nitish.singla@hmcts.net>
2021-08-27 12:10:30 +0100

    Fixing the terraform.tf

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -10,11 +10,6 @@ terraform {
       source = "hashicorp/azurerm"
       version = "~> 2.25"
     }
-  }
-}
-
-terraform {
-  required_providers {
     azuread = {
       source  = "hashicorp/azuread"
       version = "1.6.0"
