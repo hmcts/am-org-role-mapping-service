49bbbdb1c2695d6565039ac3d9f47dae8807eb5f
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2021-09-09 12:04:07 +0100

    jrd new Org role support

---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/JudicialOfficeHolder.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.orgrolemapping.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JudicialOfficeHolder implements Serializable {
+
+    private String userId; //Idam Id
+    private String office; // the business role
+    private String jurisdiction; // jurisdiction in CCD like IA
+    private List<String> authorisations; // Ticket code list from JRD
+    private ZonedDateTime beginTime; //from appointment data
+    private ZonedDateTime endTime; //from appointment data
+    private String regionId; // locationId from appointment
+    private String  baseLocationId; // epims id
+    private String primaryLocationId; //epims id where isPrimary is true
+    private String  contractType; //appointment type from JRD
+}
--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/GrantType.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/GrantType.java
@@ -1,5 +1,5 @@
 package uk.gov.hmcts.reform.orgrolemapping.domain.model.enums;
 
 public enum GrantType {
-    BASIC,SPECIFIC, STANDARD, CHALLENGED, EXCLUDED
+    BASIC, SPECIFIC, STANDARD, CHALLENGED, EXCLUDED
 }
