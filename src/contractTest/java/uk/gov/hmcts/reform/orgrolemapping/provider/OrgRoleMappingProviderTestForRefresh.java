8abb203dafe2908164dbf94e27419770fe877595
ConorH994 <conor.halpin@hmcts.net>
2022-01-13 15:09:44 +0000

    reshuffle provider pact so it isn't run during consumer stage

--- src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/provider/OrgRoleMappingProviderTestForRefresh.java
+++ src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/provider/OrgRoleMappingProviderTestForRefresh.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.orgrolemapping;
+package uk.gov.hmcts.reform.orgrolemapping.provider;
 
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
--- src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/provider/ProviderTestConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/provider/ProviderTestConfiguration.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.orgrolemapping;
+package uk.gov.hmcts.reform.orgrolemapping.provider;
 
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
