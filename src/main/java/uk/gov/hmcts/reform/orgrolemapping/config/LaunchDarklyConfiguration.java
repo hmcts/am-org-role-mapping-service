fda81d700761f742734a58a2ddd875f4651e14d5
m_2064714 <volodymyr.kovalskyi@hmcts.net>
2022-03-03 15:04:11 +0000

    LD load on startup based on yml flag

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/config/LaunchDarklyConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/config/LaunchDarklyConfiguration.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.reform.orgrolemapping.config;
 
+import com.launchdarkly.sdk.server.LDClient;
+import com.launchdarkly.sdk.server.interfaces.LDClientInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,6 +11,13 @@ import uk.gov.hmcts.reform.orgrolemapping.launchdarkly.FeatureConditionEvaluator
 
 @Configuration
 public class LaunchDarklyConfiguration implements WebMvcConfigurer {
+
+    @Value("${launchdarkly.runOnStartup:false}")
+    private boolean runOnStartup;
+
+    public LDClientInterface ldClient(@Value("${launchdarkly.sdk.key}") String sdkKey) {
+        return runOnStartup ? new LDClient(sdkKey) : new LDDummyClient();
+    }
 
     @Autowired
     private FeatureConditionEvaluator featureConditionEvaluator;
