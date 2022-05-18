f5cf082ed82f6e14488404593e3c743f24c06835
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2022-05-18 12:54:42 +0100

    added a new feature flag civil_wa_1_0

---
+++ src/main/resources/db/migration/V1.6__civil_wa_base_flag_addition.sql
@@ -0,0 +1,8 @@
+-- insert civil base flag into flag_config table
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('civil_wa_1_0', 'local', 'civil', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('civil_wa_1_0', 'pr', 'civil', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('civil_wa_1_0', 'aat', 'civil', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('civil_wa_1_0', 'demo', 'civil', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('civil_wa_1_0', 'perftest', 'civil', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('civil_wa_1_0', 'ithc', 'civil', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('civil_wa_1_0', 'prod', 'civil', 'false');
