fd255dfd1ffa27139a4ea9e391d9b64e161e4d62
ConorH994 <conor.halpin@hmcts.net>
2022-06-16 11:30:43 +0100

    remove import

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/oidc/JwtGrantedAuthoritiesConverter.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/oidc/JwtGrantedAuthoritiesConverter.java
@@ -11,7 +11,6 @@ import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames.ACCESS_TOKEN;
 
