f417934664c5f01eca485b4b615428ce831fd170
m_2064714 <volodymyr.kovalskyi@hmcts.net>
2022-07-13 10:35:11 +0100

    typo error fix

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/BulkAssignmentOrchestrator.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/BulkAssignmentOrchestrator.java
@@ -23,8 +23,8 @@ public class BulkAssignmentOrchestrator {
     //0. Orchestrator would receive list of userIds from ASB/API.
     //1. Call parse request service to extract userId List and their validations.
     //2. Call retrieveDataService to fetch the single or multiple user profiles(from CRD) and
-    // validate the data through parse request. This might require a stDroolJudicialRoleMappingCivilTest.java
-    //3. Call request mapping service to apply the mapping rules for each user
+    // validate the data through parse request. This might require a stub
+    //3. Call request mapping service to apply the mapping rules for each userBulkAssignmentOrchestrator.java
     //   a) prepare role assignment requests
     //   b)Invoke RoleAssignmentService and audit the response.
 
