579a9cff6324fb2598098782b4416ac0d5495571
ConorH994 <conor.halpin@hmcts.net>
2023-02-03 15:42:10 +0000

    remove hateoas links code

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/RoleAssignmentRequestResource.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/RoleAssignmentRequestResource.java
@@ -8,9 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.annotation.Validated;
 
-import java.util.UUID;
-
-
 @Data
 @NoArgsConstructor
 @Validated
@@ -24,14 +21,6 @@ public class RoleAssignmentRequestResource {
 
     public RoleAssignmentRequestResource(@NonNull AssignmentRequest roleAssignmentRequest) {
         this.roleAssignmentRequest = roleAssignmentRequest;
-
-        //add(linkTo(methodOn(CreateAssignmentController.class).getRoleAssignmentByActorId("")).withRel("binary"))
     }
-
-    public void addLinks(UUID documentId) {
-        LOGGER.info(" add links for document...{}", documentId);
-    }
-
-
 }
 
