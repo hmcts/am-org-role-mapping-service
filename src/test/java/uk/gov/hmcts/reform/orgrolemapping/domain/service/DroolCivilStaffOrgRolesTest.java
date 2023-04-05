adae930f0dd227e9787cb11a0f61c1266c274261
Prashanth Basavarajaiah <prashanthkumarnb@gmail.com>
2023-04-05 17:10:58 +0100

    AM-2730:Staff Civil role initial work

--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/DroolCivilStaffOrgRolesTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/DroolCivilStaffOrgRolesTest.java
@@ -132,7 +132,14 @@ class DroolCivilStaffOrgRolesTest extends DroolBase {
                         2, Arrays.asList("routine_work"))
         );
     }
-
+    static Stream<Arguments> generateDatav11() {
+        return Stream.of(
+                Arguments.of("3", Arrays.asList("hearing-centre-admin"),
+                        1, Collections.singletonList("hearing_work")),
+                Arguments.of("6", Arrays.asList("national-business-centre"),
+                        1, Arrays.asList("routine_work"))
+        );
+    }
     @ParameterizedTest
     @MethodSource("generateData")
     void shouldReturnCivilAdminMappings(String roleId, List<String> roleNames, int roleCount, List<String> workTypes) {
@@ -186,6 +193,64 @@ class DroolCivilStaffOrgRolesTest extends DroolBase {
                 workTypesCombined.add(e.entrySet().stream()
                     .filter(a -> a.getKey().equals("workTypes"))
                     .map(Map.Entry::getValue).findFirst().get().textValue());
+            }
+        }
+        assertThat(workTypesCombined,containsInAnyOrder(workTypes.toArray()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateDatav11")
+    void shouldReturnCivilAdminMappings_v11(String roleId, List<String> roleNames, int roleCount, List<String> workTypes) {
+
+        judicialAccessProfiles.clear();
+        judicialOfficeHolders.clear();
+
+        CaseWorkerAccessProfile cap = UserAccessProfileBuilder.buildUserAccessProfileForRoleId3();
+        List<String> skillCodes = List.of("civil", "test", "ctsc");
+        cap.setRoleId(roleId);
+        cap.setServiceCode("AAA6");
+        cap.setSuspended(false);
+        cap.setRegionId("region1");
+        cap.setSkillCodes(skillCodes);
+
+        allProfiles.add(cap);
+
+        //Execute Kie session
+        List<RoleAssignment> roleAssignments =
+                buildExecuteKieSession(getFeatureFlags("civil_wa_1_1", true));
+
+        //assertion
+        assertFalse(roleAssignments.isEmpty());
+        assertEquals(roleCount, roleAssignments.size());
+
+        assertThat(roleAssignments.stream().map(RoleAssignment::getRoleName).collect(Collectors.toList()),
+                containsInAnyOrder(roleNames.toArray()));
+
+        roleAssignments.forEach(r -> {
+            assertEquals("ORGANISATION", r.getRoleType().toString());
+            if (!r.getRoleName().contains("hmcts")) {
+                assertEquals(skillCodes, r.getAuthorisations());
+            }
+        });
+
+        roleAssignments.stream().filter(c -> c.getGrantType().equals(GrantType.STANDARD)).toList()
+                .forEach(r -> {
+                    assertEquals("CIVIL", r.getAttributes().get("jurisdiction").asText());
+                    if (!(roleId.equals("10") || roleId.equals("9"))) {
+                        assertEquals("region1", r.getAttributes().get("region").asText());
+                    }
+                    assertEquals(cap.getPrimaryLocationId(), r.getAttributes().get("primaryLocation").asText());
+                });
+
+        List<Map<String, JsonNode>> list = roleAssignments.stream()
+                .map(RoleAssignment::getAttributes).toList();
+        List<String> workTypesCombined = new ArrayList<>();
+
+        for (Map<String, JsonNode> e : list) {
+            if (e.containsKey("workTypes")) {
+                workTypesCombined.add(e.entrySet().stream()
+                        .filter(a -> a.getKey().equals("workTypes"))
+                        .map(Map.Entry::getValue).findFirst().get().textValue());
             }
         }
         assertThat(workTypesCombined,containsInAnyOrder(workTypes.toArray()));
