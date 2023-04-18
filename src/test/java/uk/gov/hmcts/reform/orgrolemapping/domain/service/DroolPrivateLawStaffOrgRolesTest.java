1c8f932ec7ee9558588d6d4da3c233bb81e6bc8f
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-04-18 14:26:48 +0100

    AM-2755: Add new drools rule to account for additional workType for tribunal-caseworker role

--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/DroolPrivateLawStaffOrgRolesTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/DroolPrivateLawStaffOrgRolesTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.orgrolemapping.domain.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -16,6 +17,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.CaseWorkerAccessProfile;
+import uk.gov.hmcts.reform.orgrolemapping.domain.model.FeatureFlag;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.RoleAssignment;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.enums.GrantType;
 import uk.gov.hmcts.reform.orgrolemapping.helper.UserAccessProfileBuilder;
@@ -181,9 +183,12 @@ class DroolPrivateLawStaffOrgRolesTest extends DroolBase {
 
         allProfiles.add(cap);
 
+        List<FeatureFlag> featureFlags = new ArrayList<>();
+        featureFlags.add(FeatureFlag.builder().flagName("privatelaw_wa_1_0").status(true).build());
+        featureFlags.add(FeatureFlag.builder().flagName("privatelaw_wa_1_1").status(false).build());
+
         //Execute Kie session
-        List<RoleAssignment> roleAssignments =
-                buildExecuteKieSession(getFeatureFlags("privatelaw_wa_1_0", true));
+        List<RoleAssignment> roleAssignments = buildExecuteKieSession(featureFlags);
 
         //assertion
         assertFalse(roleAssignments.isEmpty());
@@ -223,6 +228,75 @@ class DroolPrivateLawStaffOrgRolesTest extends DroolBase {
                 });
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "2,ABA5,'tribunal-caseworker',N,N",
+    })
+    void shouldReturnPrivateLawCaseWorkerMappingsUpdatedWorkTypes(String roleId,
+                                                                  String serviceCode,
+                                                                  String expectedRoles,
+                                                                  String taskSupervisorFlag,
+                                                                  String caseAllocatorFlag) {
+
+        judicialAccessProfiles.clear();
+        judicialOfficeHolders.clear();
+        List<String> skillCodes = List.of("privatelaw", "test", "ctsc");
+        CaseWorkerAccessProfile cap = UserAccessProfileBuilder.buildUserAccessProfileForRoleId2();
+        cap.setServiceCode(serviceCode);
+        cap.setSuspended(false);
+        cap.setRoleId(roleId);
+        cap.setTaskSupervisorFlag(taskSupervisorFlag);
+        cap.setCaseAllocatorFlag(caseAllocatorFlag);
+        cap.setRegionId("LDN");
+        cap.setSkillCodes(skillCodes);
+
+        allProfiles.add(cap);
+
+        List<FeatureFlag> featureFlags = new ArrayList<>();
+        featureFlags.add(FeatureFlag.builder().flagName("privatelaw_wa_1_0").status(false).build());
+        featureFlags.add(FeatureFlag.builder().flagName("privatelaw_wa_1_1").status(true).build());
+
+        //Execute Kie session
+        List<RoleAssignment> roleAssignments = buildExecuteKieSession(featureFlags);
+
+        //assertion
+        assertFalse(roleAssignments.isEmpty());
+        assertEquals(expectedRoles.split(",").length, roleAssignments.size());
+        assertThat(roleAssignments.stream().map(RoleAssignment::getRoleName).collect(Collectors.toList()),
+                containsInAnyOrder(expectedRoles.split(",")));
+        roleAssignments.forEach(r -> {
+            assertEquals("LEGAL_OPERATIONS", r.getRoleCategory().toString());
+            assertEquals("ORGANISATION", r.getRoleType().toString());
+            if (!r.getRoleName().contains("hmcts")) {
+                assertEquals(skillCodes, r.getAuthorisations());
+            }
+        });
+
+        List<String> roleNamesWithRegionAttribute = List.of("tribunal-caseworker", "senior-tribunal-caseworker",
+                "task-supervisor", "case-allocator");
+
+        roleAssignments.stream().filter(c -> c.getGrantType().equals(GrantType.STANDARD)).toList()
+                .forEach(r -> {
+                    assertEquals("PRIVATELAW", r.getAttributes().get("jurisdiction").asText());
+                    assertEquals(cap.getPrimaryLocationId(), r.getAttributes().get("primaryLocation").asText());
+                    //assert region
+                    if (roleNamesWithRegionAttribute.contains(r.getRoleName())) {
+                        assertEquals("LDN", r.getAttributes().get("region").asText());
+                    }
+                    //assert work types
+                    if (("senior-tribunal-caseworker").equals(r.getRoleName())) {
+                        assertEquals("decision_making_work",
+                                r.getAttributes().get("workTypes").asText());
+                    } else if (("tribunal-caseworker").equals(r.getRoleName())) {
+                        assertEquals("routine_work,hearing_work,applications,decision_making_work",
+                                r.getAttributes().get("workTypes").asText());
+                    } else if (Objects.equals("task-supervisor", r.getRoleName())) {
+                        assertEquals("routine_work,hearing_work,applications",
+                                r.getAttributes().get("workTypes").asText());
+                    }
+                });
+    }
+
     @Test
     void shouldNotReturnCtsRoles_disabledFlag() {
 
@@ -244,7 +318,4 @@ class DroolPrivateLawStaffOrgRolesTest extends DroolBase {
         //assertion
         assertTrue(roleAssignments.isEmpty());
     }
-
-
-
 }
