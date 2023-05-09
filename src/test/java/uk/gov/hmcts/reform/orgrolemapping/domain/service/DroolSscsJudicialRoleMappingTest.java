b82cdfab71ca31db28b9c3c29ad3dbfe347cc251
Abul Syed <abul.syed@HMCTS.net>
2023-05-09 15:44:50 +0100

    AM-2763:SSCS_WA - updated judicial role mapping tests

--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/DroolSscsJudicialRoleMappingTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/DroolSscsJudicialRoleMappingTest.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.orgrolemapping.domain.service;
 
-import org.hamcrest.collection.ArrayMatching;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.aggregator.AggregateWith;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -12,72 +10,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
 import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static uk.gov.hmcts.reform.orgrolemapping.helper.TestDataBuilder.VarargsAggregator;
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(MockitoJUnitRunner.class)
 class DroolSscsJudicialRoleMappingTest extends DroolBase {
 
     @ParameterizedTest
     @CsvSource({
-        "SSCS Tribunal Judge-Salaried,judge,case-allocator,task-supervisor,hmcts-judiciary,judge,case-allocator,"
-                 + "task-supervisor",
-        "SSCS Regional Tribunal Judge-Salaried,judge,case-allocator,task-supervisor,hmcts-judiciary,judge,"
-                + "case-allocator,task-supervisor",
-        "SSCS District Tribunal Judge-Salaried,judge,case-allocator,task-supervisor,hmcts-judiciary,judge,"
-                + "case-allocator,task-supervisor",
-        "SSCS Tribunal Judge-Salaried,judge,case-allocator,task-supervisor,hmcts-judiciary,judge,case-allocator,"
-                + "task-supervisor",
-        "SSCS Tribunal Member Medical-Salaried,medical,case-allocator,task-supervisor,hmcts-judiciary,medical,"
-                + "case-allocator,task-supervisor",
-        "SSCS Regional Medical Member-Salaried,medical,case-allocator,task-supervisor,hmcts-judiciary,medical,"
-                + "case-allocator,task-supervisor"
-    })
-    void shouldReturnSalariedRoles(String setOffice, @AggregateWith(VarargsAggregator.class) String[] roleNameOutput) {
-
-        judicialOfficeHolders.forEach(joh -> {
-            joh.setOffice(setOffice);
-            joh.setRegionId("7");
-        });
-
-        //Execute Kie session
-        List<RoleAssignment> roleAssignments =
-                buildExecuteKieSession(getFeatureFlags("sscs_wa_1_0", true));
-
-        //assertion
-        assertFalse(roleAssignments.isEmpty());
-        assertEquals(7, roleAssignments.size());
-        assertEquals(judicialOfficeHolders.stream().iterator().next().getUserId(),roleAssignments.get(0).getActorId());
-        assertEquals(judicialOfficeHolders.stream().iterator().next().getUserId(),roleAssignments.get(1).getActorId());
-        assertEquals(judicialOfficeHolders.stream().iterator().next().getUserId(),roleAssignments.get(2).getActorId());
-        assertEquals(judicialOfficeHolders.stream().iterator().next().getUserId(),roleAssignments.get(3).getActorId());
-        assertThat(roleAssignments.stream().map(RoleAssignment::getRoleName).collect(Collectors.toList()),
-                containsInAnyOrder(roleNameOutput));
-        roleAssignments.forEach(r -> {
-            assertEquals("Salaried", r.getAttributes().get("contractType").asText());
-            if (!"hmcts-judiciary".equals(r.getRoleName())) {
-                assertThat(new String[]{"7", "6"},
-                        ArrayMatching.hasItemInArray(r.getAttributes().get("region").asText()));
-            }
-        });
-
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "SSCS Tribunal Judge-Fee Paid,fee-paid-judge",
-            "SSCS Tribunal Member Medical-Fee Paid,fee-paid-medical",
-            "SSCS Tribunal Member Optometrist-Fee Paid,fee-paid-medical",
-            "SSCS Tribunal Member Disability-Fee Paid,fee-paid-disability",
-            "SSCS Tribunal Member-Fee Paid,fee-paid-disability",
-            "SSCS Tribunal Member Lay-Fee Paid,fee-paid-disability",
-            "SSCS Tribunal Member Service-Fee Paid,fee-paid-disability",
-            "SSCS Tribunal Member Financially Qualified,fee-paid-financial"
+            "SSCS President of Tribunal-Salaried,'leadership-judge,judge,case-allocator,task-supervisor,specific-access-approver-judicary,hmcts-judiciary'",
+            "SSCS Regional Tribunal Judge-Salaried,'leadership-judge,judge,case-allocator,task-supervisor,specific-access-approver-judicary,hmcts-judiciary'",
+            "SSCS Tribunal Judge-Salaried,'judge,hmcts-judiciary'",
+            "SSCS Tribunal Member Medical-Salaried,'medical,hmcts-judiciary'",
 
     })
-    void shouldReturnFeePaidRoles(String setOffice, String roleNameOutput) {
+    void shouldReturnSalariedRoles(String setOffice, String expectedRoles) {
 
         judicialOfficeHolders.forEach(joh -> joh.setOffice(setOffice));
 
@@ -87,11 +35,105 @@ class DroolSscsJudicialRoleMappingTest extends DroolBase {
 
         //assertion
         assertFalse(roleAssignments.isEmpty());
-        assertEquals(1, roleAssignments.size());
-        assertEquals(roleNameOutput,roleAssignments.get(0).getRoleName());
+        assertEquals(expectedRoles.split(",").length, roleAssignments.size());
+        assertThat(roleAssignments.stream().map(RoleAssignment::getRoleName).collect(Collectors.toList()),
+                containsInAnyOrder(expectedRoles.split(",")));
         assertEquals(judicialOfficeHolders.stream().iterator().next().getUserId(),roleAssignments.get(0).getActorId());
-        assertEquals("Fee-Paid", roleAssignments.get(0).getAttributes().get("contractType").asText());
+        boolean allAssignmentsHaveFeePaidContractType = roleAssignments.stream()
+                .allMatch(ra -> ra.getAttributes().get("contractType").asText().equals("Salaried"));
+        assertTrue(allAssignmentsHaveFeePaidContractType);
 
+        roleAssignments.forEach(r -> {
+            if (!r.getRoleName().equalsIgnoreCase("hmcts-judiciary")) {
+                assertEquals("SSCS", r.getAttributes().get("jurisdiction").asText());
+            }
+            //assert work types
+            if (("leadership-judge").equals(r.getRoleName())) {
+                assertNull(r.getAttributes().get("workTypes"));
+            } else if (("judge").equals(r.getRoleName())) {
+                assertThat(r.getAttributes().get("workTypes").asText().split(","),
+                        arrayContainingInAnyOrder("pre_hearing_work", "hearing_work",
+                                "post_hearing_work", "decision_making_work",
+                                "routine_work", "priority"));
+            } else if (("case-allocator").equals(r.getRoleName())) {
+                assertNull(r.getAttributes().get("workTypes"));
+            } else if (("task-supervisor").equals(r.getRoleName())) {
+                assertNull(r.getAttributes().get("workTypes"));
+            } else if (("specific-access-approver-judicary").equals(r.getRoleName())) {
+                assertThat(r.getAttributes().get("workTypes").asText().split(","),
+                        arrayContainingInAnyOrder("access_requests"));
+            } else if (("hmcts-judiciary").equals(r.getRoleName())) {
+                assertNull(r.getAttributes().get("workTypes"));
+            } else if (("medical").equals(r.getRoleName())) {
+                assertThat(r.getAttributes().get("workTypes").asText().split(","),
+                        arrayContainingInAnyOrder("hearing_work", "priority"));
+            }
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "SSCS Tribunal Judge-Fee Paid,'fee-paid-judge,judge,hmcts-judiciary'",
+            "SSCS Tribunal Member Medical-Fee Paid,'fee-paid-medical,hmcts-judiciary'",
+            "SSCS Tribunal Member Optometrist-Fee Paid,'fee-paid-medical,hmcts-judiciary'",
+            "SSCS Tribunal Member Disability-Fee Paid,'fee-paid-disability,hmcts-judiciary'",
+            "SSCS Tribunal Member-Fee Paid,'fee-paid-tribunal-member,hmcts-judiciary'",
+            "SSCS Tribunal Member Lay-Fee Paid,'fee-paid-tribunal-member,hmcts-judiciary'",
+            "SSCS Tribunal Member Service-Fee Paid,'fee-paid-tribunal-member,hmcts-judiciary'",
+            "SSCS Tribunal Member Financially Qualified,'fee-paid-financial,hmcts-judiciary'"
+
+    })
+    void shouldReturnFeePaidRoles(String setOffice, String expectedRoles) {
+
+        judicialOfficeHolders.forEach(joh -> joh.setOffice(setOffice));
+
+        //Execute Kie session
+        List<RoleAssignment> roleAssignments =
+                buildExecuteKieSession(getFeatureFlags("sscs_wa_1_0", true));
+
+        //assertion
+        assertFalse(roleAssignments.isEmpty());
+        assertEquals(expectedRoles.split(",").length, roleAssignments.size());
+        assertThat(roleAssignments.stream().map(RoleAssignment::getRoleName).collect(Collectors.toList()),
+                containsInAnyOrder(expectedRoles.split(",")));
+        assertEquals(judicialOfficeHolders.stream().iterator().next().getUserId(),roleAssignments.get(0).getActorId());
+        boolean allAssignmentsHaveFeePaidContractType = roleAssignments.stream()
+                .allMatch(ra -> ra.getAttributes().get("contractType").asText().equals("Fee-Paid"));
+        assertTrue(allAssignmentsHaveFeePaidContractType);
+
+        roleAssignments.forEach(r -> {
+                    if (!r.getRoleName().equalsIgnoreCase("hmcts-judiciary")) {
+                        assertEquals("SSCS", r.getAttributes().get("jurisdiction").asText());
+                    }
+                    //assert work types
+                    if (("fee-paid-judge").equals(r.getRoleName())) {
+                        assertThat(r.getAttributes().get("workTypes").asText().split(","),
+                                arrayContainingInAnyOrder("pre_hearing_work", "hearing_work",
+                                        "post_hearing_work", "decision_making_work",
+                                        "routine_work", "priority"));
+                    } else if (("judge").equals(r.getRoleName())) {
+                        assertThat(r.getAttributes().get("workTypes").asText().split(","),
+                                arrayContainingInAnyOrder("pre_hearing_work", "hearing_work",
+                                        "post_hearing_work", "decision_making_work",
+                                        "routine_work", "priority"));
+                    } else if (("hmcts-judiciary").equals(r.getRoleName())) {
+                        assertNull(r.getAttributes().get("workTypes"));
+                    } else if (("fee-paid-medical").equals(r.getRoleName())) {
+                        assertThat(r.getAttributes().get("workTypes").asText().split(","),
+                                arrayContainingInAnyOrder("hearing_work", "priority"));
+                    } else if (("fee-paid-disability").equals(r.getRoleName())) {
+                        assertThat(r.getAttributes().get("workTypes").asText().split(","),
+                                arrayContainingInAnyOrder("hearing_work", "priority"));
+                    } else if (("regional-centre-team-leader").equals(r.getRoleName())) {
+                        assertNull(r.getAttributes().get("workTypes"));
+                    } else if (("fee-paid-tribunal-member").equals(r.getRoleName())) {
+                        assertThat(r.getAttributes().get("workTypes").asText().split(","),
+                                arrayContainingInAnyOrder("hearing_work", "priority"));
+                    } else if (("fee-paid-financial").equals(r.getRoleName())) {
+                        assertThat(r.getAttributes().get("workTypes").asText().split(","),
+                                arrayContainingInAnyOrder("hearing_work", "priority"));
+                    }
+                });
     }
 
 
