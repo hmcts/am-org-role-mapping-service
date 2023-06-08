fc394b642e6794f66eb4233d0cf7f5b8eb0c4673
Mike Brown <michael.brown@hmcts.net>
2023-06-08 15:38:48 +0100

    unused imports commented for checkstyle

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/JRDTopicConsumer.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/JRDTopicConsumer.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.orgrolemapping.servicebus;
 import com.microsoft.azure.servicebus.ExceptionPhase;
 import com.microsoft.azure.servicebus.IMessage;
 import com.microsoft.azure.servicebus.IMessageHandler;
-import com.microsoft.azure.servicebus.MessageHandlerOptions;
+//import com.microsoft.azure.servicebus.MessageHandlerOptions;
 import com.microsoft.azure.servicebus.ReceiveMode;
 import com.microsoft.azure.servicebus.SubscriptionClient;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
@@ -27,8 +27,8 @@ import java.net.URISyntaxException;
 import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
