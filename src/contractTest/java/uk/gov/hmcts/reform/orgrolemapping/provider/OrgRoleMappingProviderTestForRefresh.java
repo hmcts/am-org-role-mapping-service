976f41f7403cf80d2124047938922b0fb3078b66
Mike Brown <michael.brown@hmcts.net>
2023-07-04 14:08:17 +0100

    comment added re pact.verifier.publishResults property

--- src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/provider/OrgRoleMappingProviderTestForRefresh.java
+++ src/contractTest/java/uk/gov/hmcts/reform/orgrolemapping/provider/OrgRoleMappingProviderTestForRefresh.java
@@ -80,6 +80,7 @@ public class OrgRoleMappingProviderTestForRefresh {
     @BeforeEach
     void beforeCreate(PactVerificationContext context) {
         MockMvcTestTarget testTarget = new MockMvcTestTarget();
+        //uncomment to force publishing of pacts results to central server
         //System.getProperties().setProperty("pact.verifier.publishResults", "true");
         testTarget.setControllers(new RefreshController(
                 refreshOrchestrator, judicialRefreshOrchestrator
