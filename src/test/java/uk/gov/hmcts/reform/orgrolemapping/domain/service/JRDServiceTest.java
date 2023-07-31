128a050e29abb623d913744a50cd89c0a6377a29
Mike Brown <michael.brown@hmcts.net>
2023-07-31 16:47:25 +0100

    fixes for null pointers in tests

--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/JRDServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/JRDServiceTest.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.JRDUserRequest;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.JudicialProfile;
+import uk.gov.hmcts.reform.orgrolemapping.domain.model.JudicialProfileV2;
 import uk.gov.hmcts.reform.orgrolemapping.feignclients.JRDFeignClient;
 import uk.gov.hmcts.reform.orgrolemapping.helper.TestDataBuilder;
 
@@ -44,11 +45,11 @@ class JRDServiceTest {
                 .sidamIds(Set.of("123e4567-e89b-42d3-a456-556642445000", "123e4567-e89b-42d3-a456-556642445111"))
                 .build();
 
-        JudicialProfile userProfile = TestDataBuilder.buildJudicialProfile();
+        JudicialProfileV2 userProfile = TestDataBuilder.buildJudicialProfileV2();
         doReturn(ResponseEntity.status(HttpStatus.CREATED).body(List.of(userProfile))).when(jrdFeignClient)
-                .getJudicialDetailsById(userRequest, 10);
+                .getJudicialDetailsByIdV2(userRequest, 10);
 
-        ResponseEntity<List<JudicialProfile>> responseEntity = sut.fetchJudicialProfilesV2(userRequest);
+        ResponseEntity<List<JudicialProfileV2>> responseEntity = sut.fetchJudicialProfilesV2(userRequest);
 
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
     }
