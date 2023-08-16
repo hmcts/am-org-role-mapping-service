4573c077b8b7992b3189d7578ebae43b127980bb
Mike Brown <michael.brown@hmcts.net>
2023-08-16 10:39:40 +0100

    typo fix

--- src/main/resources/db/migration/V20230816_2906__AM-2906_Enable_privatelaw_wa_1_2_Prod.sql
+++ src/main/resources/db/migration/V20230816_2906__AM-2906_Enable_privatelaw_wa_1_2_Prod.sql
@@ -1,2 +1,2 @@
 -- enable PUBLICLAW flag for: AM-2906
-update flag_config set status='true' where flag_name='publiclaw_wa_1_2' and env in ('demo', 'aat', 'perftest', 'ithc', 'prod');
+update flag_config set status='true' where flag_name='privatelaw_wa_1_2' and env in ('demo', 'aat', 'perftest', 'ithc', 'prod');
