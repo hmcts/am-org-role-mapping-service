126d3e10faa6dfd704746174bb4d4d182dbcb3b2
Mike Brown <michael.brown@hmcts.net>
2023-09-18 10:50:32 +0100

    service_name in db scripts correct to publiclaw

--- src/main/resources/db/migration/V20230912_519__COT-519_PUBLICLAW_WA_1_1_base_flag_addition.sql
+++ src/main/resources/db/migration/V20230912_519__COT-519_PUBLICLAW_WA_1_1_base_flag_addition.sql
@@ -1,7 +1,7 @@
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'local', 'st_cic', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'pr', 'st_cic', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'aat', 'st_cic', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'demo', 'st_cic', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'perftest', 'st_cic', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'ithc', 'st_cic', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'prod', 'st_cic', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'local', 'publiclaw', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'pr', 'publiclaw', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'aat', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'demo', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'perftest', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'ithc', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_1', 'prod', 'publiclaw', 'false');
