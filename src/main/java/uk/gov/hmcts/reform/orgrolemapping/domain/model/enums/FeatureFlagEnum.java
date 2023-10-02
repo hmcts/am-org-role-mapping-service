b3cc6ef1e6de99fd71abd051d68e77897f57777c
Abul Syed <abul.syed@HMCTS.net>
2023-10-02 14:40:11 +0100

    COT-484 correct feature flag enum

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/FeatureFlagEnum.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/FeatureFlagEnum.java
@@ -15,7 +15,7 @@ public enum FeatureFlagEnum {
     PRIVATELAW_WA_1_2("privatelaw_wa_1_2"),
     ST_CIC_WA_1_0("st_cic_wa_1_0"),
     PUBLICLAW_WA_1_1("publiclaw_wa_1_1"),
-    CIVIL_WA_1_2("civil_wa_1_1");
+    CIVIL_WA_1_2("civil_wa_1_2");
 
     private final String value;
 
