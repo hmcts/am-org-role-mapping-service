afda5a82a002298116af153fcba2f9f577b4c78f
Matt Nayler <matt.nayler@cgi.com>
2023-10-24 14:55:32 +0100

    AM-2902 Additional JRDService unit tests
       [AM-2902](https://tools.hmcts.net/jira/browse/AM-2902)
    
    Additional JRDService unit tests.

--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/JRDServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/JRDServiceTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.orgrolemapping.domain.service;
 
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.JRDUserRequest;
@@ -15,8 +16,11 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class JRDServiceTest {
 
@@ -41,17 +45,65 @@ class JRDServiceTest {
 
     @Test
     void fetchJudicialUserProfilesV2() throws IOException {
+
+        // GIVEN
         JRDUserRequest userRequest = JRDUserRequest.builder()
                 .sidamIds(Set.of("123e4567-e89b-42d3-a456-556642445000", "123e4567-e89b-42d3-a456-556642445111"))
                 .build();
 
         JudicialProfileV2 userProfile = TestDataBuilder.buildJudicialProfileV2();
-        doReturn(ResponseEntity.status(HttpStatus.CREATED).body(List.of(userProfile))).when(jrdFeignClient)
+        doReturn(ResponseEntity.status(HttpStatus.OK).body(List.of(userProfile))).when(jrdFeignClient)
                 .getJudicialDetailsById(userRequest, 10);
 
-        ResponseEntity<List<JudicialProfile>> responseEntity = sut.fetchJudicialProfiles(userRequest);
+        // WHEN
+        ResponseEntity<List<JudicialProfileV2>> responseEntity = sut.fetchJudicialProfiles(userRequest);
 
-        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        // THEN
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+    }
+
+    @Test
+    void fetchJudicialDetailsByServiceName() throws IOException {
+
+        // GIVEN
+        String serviceName = "test_ServiceName";
+        Integer pageSize = 123;
+        Integer pageNumber = 4;
+        String sortDirection = "test_SortDirection";
+        String sortColumn = "test_SortColumn";
+
+        JudicialProfileV2 userProfile = TestDataBuilder.buildJudicialProfileV2();
+        doReturn(ResponseEntity.status(HttpStatus.OK).body(List.of(userProfile)))
+            .when(jrdFeignClient).getJudicialDetailsByServiceName(
+                any(),
+                eq(pageSize),
+                eq(pageNumber),
+                eq(sortDirection),
+                eq(sortColumn)
+            );
+
+        // WHEN
+        ResponseEntity<List<JudicialProfileV2>> responseEntity = sut.fetchJudicialDetailsByServiceName(
+            serviceName,
+            pageSize,
+            pageNumber,
+            sortDirection,
+            sortColumn
+        );
+
+        // THEN
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        ArgumentCaptor<JRDUserRequest> captorUserRequest = ArgumentCaptor.forClass(JRDUserRequest.class);
+        verify(jrdFeignClient).getJudicialDetailsByServiceName(
+                captorUserRequest.capture(),
+                eq(pageSize),
+                eq(pageNumber),
+                eq(sortDirection),
+                eq(sortColumn)
+        );
+        assertEquals(serviceName, captorUserRequest.getValue().getCcdServiceNames());
+
     }
 
 }
