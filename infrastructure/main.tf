779672b97c1bbc23b1348801e2ebfe12c0b8a009
Mike Brown <michael.brown@hmcts.net>
2023-11-20 12:32:54 +0000

    AM-3061 v15 Reader User Trigger

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -102,6 +102,9 @@ module "org-role-mapping-database-v15" {
   common_tags        = var.common_tags
   pgsql_version      = "15"
 
+  # Setup Access Reader db user
+  force_user_permissions_trigger = "1"
+
   # The original subnet is full, this is required to use the new subnet for new databases
   subnet_suffix = "expanded"
 
