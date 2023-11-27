1109fff1b82fd20b8bd44f97396ee357b31ffc60
Mike Brown <michael.brown@hmcts.net>
2023-11-27 09:44:27 +0000

    changes copied from ts-translation-service integration test seem to be working

--- src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/controller/BaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/controller/BaseTest.java
@@ -4,18 +4,33 @@ import com.azure.messaging.servicebus.ServiceBusSenderClient;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.microsoft.azure.servicebus.SubscriptionClient;
 import com.opentable.db.postgres.embedded.EmbeddedPostgres;
-import org.junit.BeforeClass;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.TextCodec;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.orgrolemapping.OrgRoleMappingApplication;
+import uk.gov.hmcts.reform.orgrolemapping.TestIdamConfiguration;
 
 
 import javax.annotation.PreDestroy;
@@ -24,10 +39,23 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.Properties;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {
+    OrgRoleMappingApplication.class,
+    TestIdamConfiguration.class
+})
+@ActiveProfiles("itest")
+@AutoConfigureWireMock(port = 0, stubs = "classpath:/wiremock-stubs")
+@AutoConfigureMockMvc(addFilters = false)
+@SuppressWarnings({"HideUtilityClassConstructor"})
 public abstract class BaseTest {
     protected static final ObjectMapper mapper = new ObjectMapper();
 
@@ -51,11 +79,50 @@ public abstract class BaseTest {
     @MockBean(name = "getSubscriptionClient1")
     private SubscriptionClient getSubscriptionClient1;
 
-    @BeforeClass
-    public static void init() {
+    @Mock
+    protected Authentication authentication;
+
+    @BeforeEach
+    public void init() {
         mapper.registerModule(new JavaTimeModule());
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         // Force re-initialisation of base types for each test suite
+
+        Jwt jwt = dummyJwt();
+        when(authentication.getPrincipal()).thenReturn(jwt);
+        SecurityContextHolder.setContext(new SecurityContextImpl(authentication));
+    }
+
+    protected Jwt dummyJwt() {
+        return Jwt.withTokenValue("a dummy jwt token")
+                .claim("aClaim", "aClaim")
+                .header("aHeader", "aHeader")
+                .build();
+    }
+
+    protected static void stubUserInfo(String roles) {
+        final var jsonBody = "{\n"
+                + "      \"sub\": \"user@hmcts.net\",\n"
+                + "      \"uid\": \"e8275d41-7f22-4ee7-8ed3-14644d6db096\",\n"
+                + "      \"roles\": [\n"
+                + "        \"" + roles + "\"\n"
+                + "      ],\n"
+                + "      \"name\": \"Test User\",\n"
+                + "      \"given_name\": \"Test\",\n"
+                + "      \"family_name\": \"User\"\n"
+                + "    }";
+        stubFor(WireMock.get(urlEqualTo("/o/userinfo")).willReturn(
+                aResponse().withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(jsonBody)));
+    }
+
+    public static String generateDummyS2SToken(String serviceName) {
+        return Jwts.builder()
+                .setSubject(serviceName)
+                .setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, TextCodec.BASE64.encode("AA"))
+                .compact();
     }
 
     @TestConfiguration
