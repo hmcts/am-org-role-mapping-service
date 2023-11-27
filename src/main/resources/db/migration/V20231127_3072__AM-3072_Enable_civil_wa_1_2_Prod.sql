6c582ab17cfe68257593665cacceea7406c10e86
Anthony Dummer <anthony.dummer@HMCTS.net>
2023-11-27 11:16:51 +0000

    AM-3037 Enable civil_wa_1_2 ORM flag on all environments.

--- src/main/resources/db/migration/V20231127_3072__AM-3072_Enable_civil_wa_1_2_Prod.sql
+++ src/main/resources/db/migration/V20231127_3072__AM-3072_Enable_civil_wa_1_2_Prod.sql
@@ -1,2 +1,2 @@
--- enable privatelaw_wa_1_3 flag in Prod for: AM-3018 / AM-3032
+-- enable civil_wa_1_2 flag in Prod for: AM-3072
 update flag_config set status='true' where flag_name='civil_wa_1_2' and env in ('demo', 'aat', 'perftest', 'ithc', 'prod');
