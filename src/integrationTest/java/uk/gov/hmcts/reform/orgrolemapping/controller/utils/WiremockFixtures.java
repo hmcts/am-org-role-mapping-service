bb6957c93994bb75de4faf579f22d07776e417b7
Mike Brown <michael.brown@hmcts.net>
2023-12-07 13:50:40 +0000

    commented out stubs removed, unused imports removed

--- src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/controller/utils/WiremockFixtures.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/controller/utils/WiremockFixtures.java
@@ -7,28 +7,19 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.nimbusds.jose.JOSEException;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import uk.gov.hmcts.reform.orgrolemapping.domain.model.Appointment;
-import uk.gov.hmcts.reform.orgrolemapping.domain.model.AssignmentRequest;
-import uk.gov.hmcts.reform.orgrolemapping.domain.model.JudicialProfile;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.RoleAssignmentRequestResource;
 import uk.gov.hmcts.reform.orgrolemapping.helper.AssignmentRequestBuilder;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import static com.azure.core.http.ContentType.APPLICATION_JSON;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.resetAllRequests;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static uk.gov.hmcts.reform.orgrolemapping.controller.BaseTest.WIRE_MOCK_SERVER;
 import static uk.gov.hmcts.reform.orgrolemapping.util.KeyGenerator.getRsaJwk;
 
@@ -52,32 +43,6 @@ public class WiremockFixtures {
         roleAssignmentRequestResource.getRoleAssignmentRequest().getRequest().setReference("test ref");
         return roleAssignmentRequestResource;
     }
-
-//    public void stubRoleAssignmentServiceError() throws JsonProcessingException {
-//        AssignmentRequest request = AssignmentRequestBuilder.buildAssignmentRequest(false);
-//        request.getRequest().setReference("test ref");
-//
-//        WIRE_MOCK_SERVER.stubFor(post(urlEqualTo("/am/role-assignments"))
-//                .willReturn(aResponse()
-//                        .withStatus(UNPROCESSABLE_ENTITY.value())
-//                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-//                        .withBody(OBJECT_MAPPER.writeValueAsString(request))));
-//    }
-//
-//    private List<JudicialProfile> buildJudicialProfilesResponse(String userId) {
-//        List<JudicialProfile> bookings = new ArrayList<>();
-//        bookings.add(JudicialProfile.builder()
-//                .sidamId(userId)
-//                .appointments(
-//                        List.of(Appointment.builder()
-//                                .appointment("Tribunal Judge")
-//                                .appointmentType("Fee Paid")
-//                                .build()
-//                        )
-//                )
-//                .build());
-//        return bookings;
-//    }
 
     public void stubIdamConfig() throws JsonProcessingException {
 
