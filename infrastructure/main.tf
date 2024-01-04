0ecb66710fccd6249d33d9c292159c2ad13db441
Mike Brown <michael.brown@hmcts.net>
2024-01-04 14:56:52 +0000

    AM-3061 v15 Reader User Trigger 2 AAT, ITHC, Perftest, Demo

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -103,7 +103,7 @@ module "org-role-mapping-database-v15" {
   pgsql_version      = "15"
 
   # Setup Access Reader db user
-  force_user_permissions_trigger = "1"
+  force_user_permissions_trigger = "2"
 
   # The original subnet is full, this is required to use the new subnet for new databases
   subnet_suffix = "expanded"
