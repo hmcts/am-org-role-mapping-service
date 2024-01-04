837ead6c0a6649494ab33844edc43fc5901210bf
Abul Syed <abul.syed@HMCTS.net>
2024-01-04 14:52:34 +0000

    GA-49 PRM: Create table organisation_refresh_queue

---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/data/OrganisationRefreshQueue.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.orgrolemapping.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Builder(toBuilder = true)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "organisation_refresh_queue")
+public class OrganisationRefreshQueue {
+
+    @Id
+    @Column(name = "organisation_id", nullable = false)
+    private String organisationId;
+
+    @Column(name = "last_updated", nullable = false)
+    private LocalDateTime lastUpdated;
+
+    @Column(name = "access_types_min_version", nullable = false)
+    private Integer accessTypesMinVersion;
+
+    @Column(name = "active", nullable = false)
+    private Boolean active;
+}
---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/data/OrganisationRefreshQueueRepository.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.reform.orgrolemapping.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganisationRefreshQueueRepository extends CrudRepository<OrganisationRefreshQueue, String> {
+}
