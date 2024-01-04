fb42f4df12416b29de3fd567c94cf973f2e69c8d
patelila <ila.patel@hmcts.net>
2024-01-04 19:25:10 +0000

    Removed repository as discussed. Modified so only one row returned. see ticket for manual tests. Unit test tesing data

--- src/main/resources/db/migration/V20231218_47__GA-47_new_access_types_table.sql
+++ src/main/resources/db/migration/V20231218_47__GA-47_new_access_types_table.sql
@@ -6,9 +6,12 @@
 --   nextval('access_types_version_seq') NOT null
 
 CREATE TABLE access_types(
-	version bigserial not null,
+    one_row integer not null unique
+        default 1 check (one_row = 1),
+	version bigserial not null unique,
     access_types jsonb NOT NULL,
 	CONSTRAINT pk_access_types PRIMARY KEY (version)
 );
+
 
 
