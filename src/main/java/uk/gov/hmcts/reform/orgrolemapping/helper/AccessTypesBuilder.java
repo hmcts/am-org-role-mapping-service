c690122d72a74e506f9fa607e48cefdd6393a528
Abul Syed <abul.syed@HMCTS.net>
2024-01-19 17:29:01 +0000

    GA-54 fallback client

---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/helper/AccessTypesBuilder.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.orgrolemapping.helper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import uk.gov.hmcts.reform.orgrolemapping.controller.advice.exception.BadRequestException;
+import uk.gov.hmcts.reform.orgrolemapping.domain.model.AccessTypesResponse;
+
+import java.io.File;
+
+public class AccessTypesBuilder {
+
+    public static AccessTypesResponse buildAccessTypeResponse(String resource) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.readValue(
+                    new File("src/main/resources/" + resource),
+                    AccessTypesResponse.class);
+        } catch (Exception e) {
+            throw new BadRequestException("Invalid sample json is missing.");
+        }
+    }
+}
--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/CCDServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/CCDServiceTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
-public class CCDServiceTest {
+class CCDServiceTest {
 
     CCDFeignClient ccdFeignClient = mock(CCDFeignClient.class);
 
