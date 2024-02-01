b8ba7fee371b1d6506d6cdece94885a32c385987
patelila <ila.patel@hmcts.net>
2024-02-01 15:23:48 +0000

    removed the @SequenceGenerator & @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "version_seq") as we’ll be passing these values ourselves and removed implements Serializable & @Serial                                                                                                                                                                    private static final long serialVersionUID = 1L;

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/data/AccessTypesEntity.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/data/AccessTypesEntity.java
@@ -8,12 +8,8 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import java.io.Serial;
-import java.io.Serializable;
+
 
 @Builder(toBuilder = true)
 @Getter
