934a4361e9c66b3683e28ad623abc1ea23c52c8c
patelila <ila.patel@hmcts.net>
2024-02-05 14:55:02 +0000

    Removed @@UpdateTimestamp and changes String to long.                                                                                                                                                                    private static final long serialVersionUID = 1L;

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/data/BatchLastRunTimestampEntity.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/data/BatchLastRunTimestampEntity.java
@@ -5,7 +5,6 @@ import lombok.Setter;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.SequenceGenerator;
@@ -30,14 +29,13 @@ public class BatchLastRunTimestampEntity  {
     @Id
     @Column(name = "id", updatable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "batch_last_run_timestamp_id_seq")
-    private String batchLastRunTimestampId;
+    private long batchLastRunTimestampId;
 
     //e.g. 2000-01-01T00:00:00
     // JDBC and JPA 2.2 you are supposed to map it to java.time.OffsetDateTime
     // (or to java.util.Date or java.sql.Timestamp of course)
     @Column(name = "last_user_run_date_time", nullable = false)
     @Temporal(TemporalType.DATE)
-    @UpdateTimestamp
     private Date lastUserRunDatetime;
 
     //e.g. 2000-01-01T00:00:00
@@ -45,7 +43,6 @@ public class BatchLastRunTimestampEntity  {
     // (or to java.util.Date or java.sql.Timestamp of course)
     @Column(name = "last_organisation_run_date_time", nullable = false)
     @Temporal(TemporalType.DATE)
-    @UpdateTimestamp
     private Date lastOrganisationRunDatetime;
 
 }
