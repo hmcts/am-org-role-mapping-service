1f1dadd0c2dcb9ae9497937acd951559f52ec052
mikebrownccd <104495891+mikebrownccd@users.noreply.github.com>
2024-02-19 11:41:22 +0000

    DTSAM-131 CIVIL_WA_1_4 remove region (#1751)
    * DTSAM-131 CIVIL_WA_1_4 - Drool rules updated, flag added, all tests passing
    
    * expectedRoleNameWorkTypesMap added, tests updated and all passing
    
    * test refactor
    
    * v1.4 task-supervisor and case-allocator drool rules added, further test refactoring
    
    * 'region' removed for expected FT F007 S-063 response to match new behaviour
    
    * comments added to help rule retirement
    
    * comments added to help rule retirement
    
    * region assertion fixed, parameterized tests reprdered to match config page, parameters list comment added
    
    * @S-063 feature toggle updated to @FeatureToggle(DB:civil_wa_1_4=on)

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/FeatureFlagEnum.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/FeatureFlagEnum.java
@@ -18,7 +18,8 @@ public enum FeatureFlagEnum {
     CIVIL_WA_1_2("civil_wa_1_2"),
     CIVIL_WA_1_3("civil_wa_1_3"),
     PRIVATELAW_WA_1_3("privatelaw_wa_1_3"),
-    PUBLICLAW_WA_1_2("publiclaw_wa_1_2");
+    PUBLICLAW_WA_1_2("publiclaw_wa_1_2"),
+    CIVIL_WA_1_4("civil_wa_1_4");
 
     private final String value;
 
