6ba2e25283cb46e54ab66f0738a68b585b23168f
Matt Nayler <matt.nayler@cgi.com>
2024-02-26 19:14:07 +0000

    DTSAM-150 Adjust swagger for TopicController
       [DTSAM-150](https://tools.hmcts.net/jira/browse/DTSAM-150)
    
    Adjust swagger for TopicController.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/TopicController.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/TopicController.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.reform.orgrolemapping.servicebus;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.NoArgsConstructor;
@@ -12,9 +14,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.orgrolemapping.domain.model.UserRequest;
 
 import static uk.gov.hmcts.reform.orgrolemapping.apihelper.Constants.AUTHORIZATION;
 import static uk.gov.hmcts.reform.orgrolemapping.apihelper.Constants.SERVICE_AUTHORIZATION;
@@ -36,46 +38,52 @@ public class TopicController {
     }
 
     @PostMapping(
-            path = "/am/testing-support/send2CrdTopic",
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = {"application/json"}
+        path = "/am/testing-support/send2CrdTopic",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = {"application/json"}
     )
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    @Operation(summary = "sends message to the CRD topic in Azure Service Bus",
-            security =
-            {
-                @SecurityRequirement(name = AUTHORIZATION),
-                @SecurityRequirement(name = SERVICE_AUTHORIZATION)
-            })
-    @ApiResponse(
-            responseCode = "204",
-            description = "No Content",
-            content = @Content()
+    @Operation(
+        summary = "sends message to the CRD topic in Azure Service Bus",
+        security = {
+            @SecurityRequirement(name = AUTHORIZATION),
+            @SecurityRequirement(name = SERVICE_AUTHORIZATION)
+        }
     )
-    public ResponseEntity<Void> send2CRD(@RequestBody String body) {
+    @ApiResponse(
+        responseCode = "204",
+        description = "No Content",
+        content = @Content()
+    )
+    public ResponseEntity<Void> send2CRD(
+        @RequestBody(content = @Content(schema = @Schema(implementation = UserRequest.class))) String body
+    ) {
         log.info("Sending message 2 CRD topic");
         crdTopicPublisher.sendMessage(body);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping(
-            path = "/am/testing-support/send2JrdTopic",
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = {"application/json"}
+        path = "/am/testing-support/send2JrdTopic",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = {"application/json"}
     )
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    @Operation(summary = "sends message to the JRD topic in Azure Service Bus",
-            security =
-            {
-                @SecurityRequirement(name = AUTHORIZATION),
-                @SecurityRequirement(name = SERVICE_AUTHORIZATION)
-            })
-    @ApiResponse(
-            responseCode = "204",
-            description = "No Content",
-            content = @Content()
+    @Operation(
+        summary = "sends message to the JRD topic in Azure Service Bus",
+        security = {
+            @SecurityRequirement(name = AUTHORIZATION),
+            @SecurityRequirement(name = SERVICE_AUTHORIZATION)
+        }
     )
-    public ResponseEntity<Void> send2JRD(@RequestBody String body) {
+    @ApiResponse(
+        responseCode = "204",
+        description = "No Content",
+        content = @Content()
+    )
+    public ResponseEntity<Void> send2JRD(
+        @RequestBody(content = @Content(schema = @Schema(implementation = UserRequest.class))) String body
+    ) {
         log.info("Sending message 2 JRD topic");
         jrdTopicPublisher.sendMessage(body);
         return ResponseEntity.noContent().build();
