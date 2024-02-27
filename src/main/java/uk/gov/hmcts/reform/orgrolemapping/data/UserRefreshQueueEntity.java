e23a8b985da3d537ece926b039ec880dd950b2a5
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-02-27 16:59:12 +0000

    fix styles

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/data/UserRefreshQueueEntity.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/data/UserRefreshQueueEntity.java
@@ -7,7 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Builder(toBuilder = true)
