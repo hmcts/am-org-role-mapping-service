f6a09697f00ad7c9ab9915584f5955394435124e
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2024-02-28 10:37:33 +0000

    GA-56: added orgLastUpdated from ga 137

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/constants/PrmConstants.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/constants/PrmConstants.java
@@ -6,6 +6,7 @@ public class PrmConstants {
 
     public static final String ORGANISATION_ID = "organisationId";
     public static final String LAST_UPDATED = "lastUpdated";
+    public static final String ORGANISATION_LAST_UPDATED = "organisationLastUpdated";
     public static final String ACCESS_TYPES_MIN_VERSION = "accessTypesMinVersion";
     public static final String SINCE_TIMESTAMP_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
     public static final DateTimeFormatter ISO_DATE_TIME_FORMATTER = DateTimeFormatter
