510a3981429d991cdfdd1b92d813bd6984eb93f8
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-02-28 16:34:48 +0000

    Revert "GA-137 update tests"
    This reverts commit b3c892e3633e30425a49e9f77da2d2ac45739826.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/OrganisationInfo.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/OrganisationInfo.java
@@ -16,6 +16,6 @@ public class OrganisationInfo {
 
     private String organisationIdentifier;
     private String status;
-    private LocalDateTime lastUpdated;
+    private LocalDateTime organisationLastUpdated;
     private List<String> organisationProfileIds;
 }
