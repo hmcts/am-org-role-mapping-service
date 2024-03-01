2a2306a6587210c05418e7f1946682994c069f99
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-03-01 10:04:05 +0000

    removed unused import

--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/monitoring/models/ProcessMonitorDtoTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/monitoring/models/ProcessMonitorDtoTest.java
@@ -3,8 +3,6 @@ package uk.gov.hmcts.reform.orgrolemapping.monitoring.models;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
