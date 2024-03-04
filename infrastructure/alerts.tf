6d4758ea21e6db0b9f7ab1efd0258e83d788d315
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-03-04 13:55:12 +0000

    Update process names and alert names in infrastructure/alerts.tf

--- infrastructure/alerts.tf
+++ infrastructure/alerts.tf
@@ -1,37 +1,44 @@
 variable "process_names" {
   description = "A list of process names"
   type = list(object({
+    key = string
     frequencyInMinutes = number
     timeWindowInMinutes = number
     processName = string
   }))
   default = [
     {
+      key = "prm-process-1"
       frequencyInMinutes    = 15
       timeWindowInMinutes   = 10080
       processName           = "PRM Process 1"
     },
     {
+      key = "prm-process-2"
       frequencyInMinutes    = 15
       timeWindowInMinutes   = 10080
       processName           = "PRM Process 2"
     },
     {
+      key = "prm-process-3"
       frequencyInMinutes    = 15
       timeWindowInMinutes   = 10080
       processName           = "PRM Process 3"
     },
     {
+      key = "prm-process-4"
       frequencyInMinutes    = 15
       timeWindowInMinutes   = 10080
       processName           = "PRM Process 4"
     },
     {
+      key = "prm-process-5"
       frequencyInMinutes    = 15
       timeWindowInMinutes   = 10080
       processName           = "PRM Process 5"
     },
     {
+      key = "prm-process-6"
       frequencyInMinutes    = 15
       timeWindowInMinutes   = 10080
       processName           = "PRM Process 6"
@@ -42,13 +49,13 @@ variable "process_names" {
 module "prm-process-not-started-alerts" {
   source = "git@github.com:hmcts/cnp-module-metric-alert"
 
-  for_each = { for process in var.process_names : process.processName => process }
+  for_each = { for process in var.process_names : process.key => process }
 
   location = var.location
  
   app_insights_name = join("-", ["am", local.local_env])
  
-  alert_name = "${each.value.processName} - Not started"
+  alert_name = "am-${each.value.key}-not-started-alert"
   alert_desc = "Triggers when ${each.value.processName} has not started the expected time frame in am-${local.local_env}."
   app_insights_query = "customEvents | where name == '${each.value.processName} - Started' | limit 1"
   custom_email_subject = "Alert: ${each.value.processName} has not started in expected time frame in am-${local.local_env}"
@@ -66,13 +73,13 @@ module "prm-process-not-started-alerts" {
 module "prm-process-not-completed-alerts" {
   source = "git@github.com:hmcts/cnp-module-metric-alert"
 
-  for_each = { for process in var.process_names : process.processName => process }
+  for_each = { for process in var.process_names : process.key => process }
 
   location = var.location
  
   app_insights_name = join("-", ["am", local.local_env])
  
-  alert_name = "${each.value.processName} - Not completed"
+  alert_name = "am-${each.value.key}-not-completed-alert"
   alert_desc = "Triggers when ${each.value.processName} has started but not completed am-${local.local_env}."
   app_insights_query = "customEvents | where name startswith '${each.value.processName}' | where timestamp >= ago(60m) | order by timestamp asc | extend prevName = prev(name) | where prevName == '${each.value.processName} - Started' and name == '${each.value.processName}' - Completed'"
   custom_email_subject = "Alert: ${each.value.processName} has not completed am-${local.local_env}"
@@ -90,13 +97,13 @@ module "prm-process-not-completed-alerts" {
 module "prm-process-failure-alerts" {
   source = "git@github.com:hmcts/cnp-module-metric-alert"
 
-  for_each = { for process in var.process_names : process.processName => process }
+  for_each = { for process in var.process_names : process.key => process }
 
   location = var.location
  
   app_insights_name = join("-", ["am", local.local_env])
  
-  alert_name = "${each.value.processName} - Failed"
+  alert_name = "am-${each.value.key}-failure-alert"
   alert_desc = "Triggers when ${each.value.processName} fails am-${local.local_env}."
   app_insights_query = "customEvents | where name == '${each.value.processName} - Failed' | where timestamp >= ago(15m) | sort by timestamp desc"
   custom_email_subject = "Alert: ${each.value.processName} failed in am-${var.env}"
