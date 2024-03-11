a72bfa78f216a5eae7bb31395423de5c2fd616d4
Mike Brown <michael.brown@hmcts.net>
2024-03-11 15:03:39 +0000

    message processing and error logic moved to new TopicConsumer

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/JRDTopicConsumerNew.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/JRDTopicConsumerNew.java
@@ -1,52 +1,24 @@
 package uk.gov.hmcts.reform.orgrolemapping.servicebus;
 
-import com.azure.messaging.servicebus.ServiceBusErrorContext;
 import com.azure.messaging.servicebus.ServiceBusProcessorClient;
-import com.azure.messaging.servicebus.ServiceBusReceivedMessageContext;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
-import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.instrumentation.annotations.WithSpan;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.orgrolemapping.controller.advice.exception.InvalidRequest;
-import uk.gov.hmcts.reform.orgrolemapping.domain.model.UserRequest;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.enums.UserType;
-import uk.gov.hmcts.reform.orgrolemapping.domain.service.BulkAssignmentOrchestrator;
-import uk.gov.hmcts.reform.orgrolemapping.servicebus.deserializer.OrmDeserializer;
-
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 
 @Slf4j
 @Component
 public class JRDTopicConsumerNew extends JRDMessagingConfiguration {
 
-    private BulkAssignmentOrchestrator bulkAssignmentOrchestrator;
-    private OrmDeserializer deserializer;
+    private TopicConsumer topicConsumer;
 
-    public JRDTopicConsumerNew(BulkAssignmentOrchestrator bulkAssignmentOrchestrator,
-                               OrmDeserializer deserializer) {
-        this.bulkAssignmentOrchestrator = bulkAssignmentOrchestrator;
-        this.deserializer = deserializer;
+    public JRDTopicConsumerNew(TopicConsumer topicConsumer) {
+        this.topicConsumer = topicConsumer;
     }
-
-    Consumer<ServiceBusReceivedMessageContext> processMessage = messageContext -> {
-        System.out.println(messageContext.getMessage().getMessageId());
-        byte[] body = messageContext.getMessage().getBody().toBytes();
-        processMessage(body);
-        // other message processing code
-    };
-
-    Consumer<ServiceBusErrorContext> processError = errorContext -> {
-        Exception e = (Exception) errorContext.getException();
-        log.error("Error processing JRD message from service bus : {}", e.getMessage());
-        throw new InvalidRequest("Error processing message from service bus", e);
-    };
 
     @Bean
     @Qualifier("jrdConsumer")
@@ -56,7 +28,9 @@ public class JRDTopicConsumerNew extends JRDMessagingConfiguration {
             throws ServiceBusException, InterruptedException {
 
         log.error("Inside registerJRDMessageHandlerOnClient Before processorClient");
-        ServiceBusProcessorClient processorClient = getServiceBusProcessorClient(processMessage, processError);
+        ServiceBusProcessorClient processorClient = getServiceBusProcessorClient(
+                messageContext -> topicConsumer.processMessage(messageContext, UserType.JUDICIAL),
+                TopicConsumer::processError);
 
         log.error("Inside registerJRDMessageHandlerOnClient after processorClient");
         // Starts the processor in the background and returns immediately
@@ -64,17 +38,6 @@ public class JRDTopicConsumerNew extends JRDMessagingConfiguration {
         log.error("Inside registerJRDMessageHandlerOnClient after start");
 
         return null;
-    }
-
-    @WithSpan(value = "JRD Azure Service Bus Topic AzureV2", kind = SpanKind.CONSUMER)
-    private void processMessage(byte[] body) {
-        UserRequest request = deserializer.deserializeBytes(body);
-        log.debug("Parsing the message from JRD with size :: {}", request.getUserIds().size());
-
-        ResponseEntity<Object> response = bulkAssignmentOrchestrator.createBulkAssignmentsRequest(request,
-                UserType.JUDICIAL);
-
-        log.debug("Role Assignment Service Response JRD: {}", response.getStatusCode());
     }
 
 }
---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/TopicConsumer.java
@@ -0,0 +1,72 @@
+package uk.gov.hmcts.reform.orgrolemapping.servicebus;
+
+import com.azure.messaging.servicebus.ServiceBusErrorContext;
+import com.azure.messaging.servicebus.ServiceBusException;
+import com.azure.messaging.servicebus.ServiceBusFailureReason;
+import com.azure.messaging.servicebus.ServiceBusReceivedMessageContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.orgrolemapping.domain.model.UserRequest;
+import uk.gov.hmcts.reform.orgrolemapping.domain.model.enums.UserType;
+import uk.gov.hmcts.reform.orgrolemapping.domain.service.BulkAssignmentOrchestrator;
+import uk.gov.hmcts.reform.orgrolemapping.servicebus.deserializer.OrmDeserializer;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Slf4j
+public class TopicConsumer {
+
+    private BulkAssignmentOrchestrator bulkAssignmentOrchestrator;
+    private OrmDeserializer deserializer;
+
+    public TopicConsumer(BulkAssignmentOrchestrator bulkAssignmentOrchestrator,
+                               OrmDeserializer deserializer) {
+        this.bulkAssignmentOrchestrator = bulkAssignmentOrchestrator;
+        this.deserializer = deserializer;
+    }
+
+    public static void processError(ServiceBusErrorContext context) {
+        log.error("Error when receiving messages from namespace: '{}'. Entity: '{}'",
+                context.getFullyQualifiedNamespace(), context.getEntityPath());
+
+        if (!(context.getException() instanceof ServiceBusException)) {
+            log.error("Non-ServiceBusException occurred: {}", context.getException());
+            return;
+        }
+
+        ServiceBusException exception = (ServiceBusException) context.getException();
+        ServiceBusFailureReason reason = exception.getReason();
+
+        if (reason == ServiceBusFailureReason.MESSAGING_ENTITY_DISABLED
+                || reason == ServiceBusFailureReason.MESSAGING_ENTITY_NOT_FOUND
+                || reason == ServiceBusFailureReason.UNAUTHORIZED) {
+            log.error("An unrecoverable error occurred. Stopping processing with reason {}: {}",
+                    reason, exception.getMessage());
+        } else if (reason == ServiceBusFailureReason.MESSAGE_LOCK_LOST) {
+            log.error("Message lock lost for message: {}", context.getException());
+        } else if (reason == ServiceBusFailureReason.SERVICE_BUSY) {
+            try {
+                // Choosing an arbitrary amount of time to wait until trying again.
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                log.error("Unable to sleep for period of time");
+            }
+        } else {
+            log.error("Error source {}, reason {}, message: {}", context.getErrorSource(),
+                    reason, context.getException());
+        }
+    }
+
+    public void processMessage(ServiceBusReceivedMessageContext messageContext, UserType userType) {
+        System.out.println(messageContext.getMessage().getMessageId());
+        byte[] body = messageContext.getMessage().getBody().toBytes();
+        UserRequest request = deserializer.deserializeBytes(body);
+        log.debug("Parsing the message from JRD with size :: {}", request.getUserIds().size());
+
+        ResponseEntity<Object> response = bulkAssignmentOrchestrator.createBulkAssignmentsRequest(request, userType);
+
+        log.debug("Role Assignment Service Response JRD: {}", response.getStatusCode());
+    }
+}
