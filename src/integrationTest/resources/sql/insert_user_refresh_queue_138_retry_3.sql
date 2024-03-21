5d6f3fa588717a310fdb57fb28b2b506b3f006d5
Helen Bird <helen.bird@HMCTS.NET>
2024-03-21 17:17:38 +0000

    GA-53 Amendments

---
+++ src/integrationTest/resources/sql/insert_user_refresh_queue_138_retry_3.sql
@@ -0,0 +1,21 @@
+DELETE FROM access_types;
+DELETE FROM user_refresh_queue;
+
+INSERT INTO public.access_types (access_types, version)
+VALUES('{ "organisationProfiles":
+[{"organisationProfileId": "SOLICITOR_PROFILE",
+    "jurisdictions":
+    [{"jurisdictionId": "BEFTA_JURISDICTION_1",
+        "accessTypes": [{"accessTypeId": "1","accessMandatory": true,"accessDefault": true,
+            "roles": [{"caseTypeId": "23","organisationalRoleName": "organisationRoleName1"}]}]},
+     {"jurisdictionId": "BEFTA_JURISDICTION_2",
+                  "accessTypes": [{"accessTypeId": "2","accessMandatory": true,"accessDefault": true,
+             "roles": [{"caseTypeId": "23","groupRoleName": "groupname2","caseGroupIdTemplate": "IA:all:IA:AS1:$ORGID$","groupAccessEnabled": true}]}]}
+                     ]}]}'::jsonb, 2);
+
+INSERT INTO public.user_refresh_queue (user_id, user_last_updated,last_updated, access_types_min_version, deleted, access_types, organisation_id, organisation_status, organisation_profile_ids, active, retry)
+VALUES('1', now(), now(), 1, null, '[{ "jurisdictionId": "BEFTA_JURISDICTION_1","organisationProfileId": "SOLICITOR_PROFILE","accessTypeId": "1","enabled": true} ,{ "jurisdictionId": "BEFTA_JURISDICTION_2","organisationProfileId": "SOLICITOR_PROFILE","accessTypeId": "2","enabled": true}]'::jsonb, 'OrgId', 'ACTIVE', '{"SOLICITOR_PROFILE","2"}', true, 3);
+
+
+
+
