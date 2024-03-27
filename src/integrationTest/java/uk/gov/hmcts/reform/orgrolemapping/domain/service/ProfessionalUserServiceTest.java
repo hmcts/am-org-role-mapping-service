f07f542b22fda70907d40607b6cd21045cb9669d
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-03-27 17:44:50 +0000

    GA-58 update assertion to ensure correct values are used for P5

--- src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/ProfessionalUserServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/ProfessionalUserServiceTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
 import uk.gov.hmcts.reform.orgrolemapping.controller.BaseTestIntegration;
+import uk.gov.hmcts.reform.orgrolemapping.data.BatchLastRunTimestampEntity;
+import uk.gov.hmcts.reform.orgrolemapping.data.BatchLastRunTimestampRepository;
 import uk.gov.hmcts.reform.orgrolemapping.data.UserRefreshQueueRepository;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.GetRefreshUserResponse;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.RefreshUser;
@@ -13,10 +15,15 @@ import uk.gov.hmcts.reform.orgrolemapping.helper.IntTestDataBuilder;
 
 import javax.transaction.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.orgrolemapping.helper.IntTestDataBuilder.refreshUser;
 
@@ -28,6 +35,9 @@ public class ProfessionalUserServiceTest extends BaseTestIntegration {
 
     @Autowired
     private UserRefreshQueueRepository userRefreshQueueRepository;
+
+    @Autowired
+    private BatchLastRunTimestampRepository batchLastRunTimestampRepository;
 
     @MockBean
     private PrdService prdService;
@@ -55,6 +65,8 @@ public class ProfessionalUserServiceTest extends BaseTestIntegration {
                        "classpath:sql/insert_batch_last_run.sql",
                        "classpath:sql/insert_user_refresh_queue.sql"})
     void shouldFindUserChangesAndInsertIntoRefreshQueue_WithPagination() {
+        final LocalDateTime preTestLastBatchRunTime = getLastUserRunDatetime();
+
         RefreshUser refreshUser = refreshUser(1);
         GetRefreshUserResponse response1 = IntTestDataBuilder.buildRefreshUserResponse(refreshUser, "123", true);
 
@@ -70,5 +82,22 @@ public class ProfessionalUserServiceTest extends BaseTestIntegration {
         professionalUserService.findUserChangesAndInsertIntoUserRefreshQueue();
 
         assertEquals(3, userRefreshQueueRepository.findAll().size());
+
+        LocalDateTime postTestLastBatchRunTime = getLastUserRunDatetime();
+
+        // assert the last batch run time has been updated
+        assertTrue(postTestLastBatchRunTime.isAfter(preTestLastBatchRunTime));
+
+        // assert prd service is invoked with the original last user run time (value from sql script, not to be
+        // confused with last org run time)
+        verify(prdService).retrieveUsers(eq("2024-02-01T12:31:56"), anyInt(), eq(null));
+        verify(prdService).retrieveUsers(eq("2024-02-01T12:31:56"), anyInt(), eq("123"));
+    }
+
+    private LocalDateTime getLastUserRunDatetime() {
+        List<BatchLastRunTimestampEntity> allBatchLastRunTimestampEntities = batchLastRunTimestampRepository
+                .findAll();
+        BatchLastRunTimestampEntity batchLastRunTimestampEntity = allBatchLastRunTimestampEntities.get(0);
+        return batchLastRunTimestampEntity.getLastUserRunDatetime();
     }
 }
