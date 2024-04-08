bd5b2c6b94cc4924d9f9e50bcef34f5579f336cc
Mike Brown <michael.brown@hmcts.net>
2024-04-08 18:35:18 +0100

    unused import removed

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/feignclients/configuration/RdFeignClientInterceptor.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/feignclients/configuration/RdFeignClientInterceptor.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.orgrolemapping.feignclients.configuration;
 import feign.RequestInterceptor;
 import org.apache.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.orgrolemapping.apihelper.Constants;
