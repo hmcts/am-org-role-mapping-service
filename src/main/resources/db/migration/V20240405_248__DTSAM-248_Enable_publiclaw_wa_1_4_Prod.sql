62d97a6dbed85a2592e0e6c69625c789a8177d70
Mike Brown <michael.brown@hmcts.net>
2024-04-08 11:30:09 +0100

    DTSAM-248 correct 'publiclaw' to 'privatelaw' in file name, sql, and comment

--- src/main/resources/db/migration/V20240405_248__DTSAM-248_Enable_publiclaw_wa_1_4_Prod.sql
+++
@@ -1,2 +0,0 @@
--- enable publiclaw_wa_1_4 flag in Prod for: DTSAM-248
-update flag_config set status='true' where flag_name='publiclaw_wa_1_4' and env in ('demo', 'aat', 'perftest', 'ithc', 'prod');
