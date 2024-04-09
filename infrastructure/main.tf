40b8b2f96f312b0edc01ed231fd392aefa6ccf71
Mike Brown <michael.brown@hmcts.net>
2024-04-09 11:51:08 +0100

    AM-3036 Schema Owner Trigger Prod

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -109,7 +109,7 @@ module "org-role-mapping-database-v15" {
   pgsql_version      = "15"
 
   # Setup Access Reader db user
-  force_user_permissions_trigger = "3"
+  force_user_permissions_trigger = "4"
 
   # Sets correct DB owner after migration to fix permissions
   enable_schema_ownership = var.enable_schema_ownership
