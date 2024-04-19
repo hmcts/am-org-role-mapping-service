6483fb9cb0257f93b044961fba213e924a64bf05
Abul Syed <abul.syed@HMCTS.net>
2024-04-19 14:45:09 +0100

    DTSAM-265 remove region from SSCS dwp hmrc

---
+++ src/main/resources/db/migration/V20240419_265__DTSAM-265_SSCS_WA_1_2_base_flag_addition.sql
@@ -0,0 +1,7 @@
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('sscs_wa_1_2', 'local', 'employment', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('sscs_wa_1_2', 'pr', 'employment', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('sscs_wa_1_2', 'aat', 'employment', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('sscs_wa_1_2', 'demo', 'employment', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('sscs_wa_1_2', 'perftest', 'employment', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('sscs_wa_1_2', 'ithc', 'employment', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('sscs_wa_1_2', 'prod', 'employment', 'false');
