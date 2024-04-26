fb2bfb98016b6132a98f544da9f47588d60e557e
Matt Nayler <57350764+mattnayler@users.noreply.github.com>
2024-04-26 15:42:11 +0100

    DTSAM-165 RefreshJobs Test Controller swagger (#1890)
    [DTSAM-165](https://tools.hmcts.net/jira/browse/DTSAM-165)
    
    Adjust the RefreshJobsController swagger output.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/controller/testingsupport/RefreshJobsController.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/controller/testingsupport/RefreshJobsController.java
@@ -1,8 +1,10 @@
 package uk.gov.hmcts.reform.orgrolemapping.controller.testingsupport;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,12 +22,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.orgrolemapping.controller.testingsupport.domain.RefreshJob;
 import uk.gov.hmcts.reform.orgrolemapping.data.RefreshJobEntity;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.UserRequest;
 import uk.gov.hmcts.reform.orgrolemapping.domain.service.PersistenceService;
 
+import java.time.ZonedDateTime;
 import java.util.Optional;
+
+import static uk.gov.hmcts.reform.orgrolemapping.util.JacksonUtils.MAPPER;
 
 @RestController
 @Slf4j
@@ -39,29 +47,43 @@ public class RefreshJobsController {
         this.persistenceService = persistenceService;
     }
 
-    @Operation(summary = "Create a new Refresh Job ", description = "To Create Refresh Job for actod/IDAM ID's"
-            + " passed in the Body of the request and "
-            + " roleCategory(JUDICIAL, LEGAL_OPERATIONS) "
-            + " jurisdiction passed as Request Params"
-            + " linkJob set to false if you want to refresh by service(default is TRUE)")
+    @Operation(summary = "Create a new Refresh Job ")
     @PostMapping(
             path = "/am/testing-support/job",
             consumes = {"application/json"}
     )
+    @ResponseStatus(code = HttpStatus.CREATED)
     @ApiResponse(
-            responseCode = "202",
-            description = "Accepted",
-            content = @Content()
+            responseCode = "201",
+            description = "Created",
+            content = { @Content(schema = @Schema(implementation = RefreshJob.class), mediaType = "application/json") }
     )
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public ResponseEntity<RefreshJobEntity> insertJob(
+    public ResponseEntity<RefreshJob> insertJob(
+            @Parameter(description
+                    = "The Role Category: one of `JUDICIAL` or `LEGAL_OPERATIONS` for a caseworker refresh.")
             @RequestParam String roleCategory,
+            @Parameter(description = "The Jurisdiction / Service Name to use in a search based refresh.")
             @RequestParam String jurisdiction,
+            @Parameter(description
+                = "`true` will auto assign the `linkedJobId` value ready for a targeted refresh of users.  "
+                + "Set to `false` if you want to refresh all users in the given jurisdiction."
+            )
             @RequestParam(required = false, defaultValue = "true") Boolean linkJob,
-            @RequestParam(required = false) Long linkedJobId,
+            @Parameter(description
+                    = "The Job ID of the refresh job entry to 're-run', i.e. where to source User ID values from.  "
+                    + "Will be auto set to the new Job ID if `linkJob=true`.")
+            @RequestParam(required = false, defaultValue = "0") Long linkedJobId,
+            @Parameter(description
+                = "The status of the Job. This will default to `NEW`.  "
+                + "Other recognised status values include: `ABORTED` and `COMPLETED`.")
             @RequestParam(required = false) String status,
             @RequestParam(required = false) String comments,
+            @Nullable
             @RequestBody(required = false) UserRequest userRequest) {
+        if (userRequest == null) {
+            userRequest = new UserRequest(); // default it to empty object
+        }
         RefreshJobEntity newJob = RefreshJobEntity.builder()
                 .jurisdiction(jurisdiction)
                 .roleCategory(roleCategory)
@@ -70,32 +92,39 @@ public class RefreshJobsController {
                         ? userRequest.getUserIds().toArray(new String[0]) : null)
                 .linkedJobId(linkedJobId)
                 .comments(comments)
+                .created(ZonedDateTime.now())
                 .build();
         newJob = persistenceService.persistRefreshJob(newJob);
         if (Boolean.TRUE.equals(linkJob)) {
             newJob.setLinkedJobId(newJob.getJobId());
             newJob = persistenceService.persistRefreshJob(newJob);
         }
-        return ResponseEntity.status(HttpStatus.OK).body(newJob);
+        return ResponseEntity.status(HttpStatus.CREATED).body(convertRefreshJob(newJob));
     }
 
     @Operation(summary = "Get a Refresh Job")
     @GetMapping(value = "/am/testing-support/jobs/{jobId}")
+    @ResponseStatus(code = HttpStatus.OK)
     @ApiResponse(
             responseCode = "200",
             description = "Successful",
+            content = { @Content(schema = @Schema(implementation = RefreshJob.class), mediaType = "application/json") }
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "Refresh Job not found",
             content = @Content()
     )
-    public ResponseEntity<RefreshJobEntity>  fetchJob(@Parameter(required = true) @PathVariable("jobId") Long jobId) {
+    public ResponseEntity<RefreshJob>  fetchJob(@Parameter(required = true) @PathVariable("jobId") Long jobId) {
         Optional<RefreshJobEntity> refreshJobEntity = persistenceService.fetchRefreshJobById(jobId);
-        if (refreshJobEntity.isPresent()) {
-            return ResponseEntity.status(HttpStatus.OK).body(refreshJobEntity.get());
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return refreshJobEntity
+                .map(jobEntity -> ResponseEntity.status(HttpStatus.OK).body(convertRefreshJob(jobEntity)))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @Operation(summary = "Delete a Refresh Job")
     @DeleteMapping(value = "/am/testing-support/jobs/{jobId}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @ApiResponse(
             responseCode = "204",
             description = "No Content",
@@ -103,11 +132,12 @@ public class RefreshJobsController {
     )
     public ResponseEntity<Void>  removeJob(@Parameter(required = true) @PathVariable("jobId") Long jobId) {
         Optional<RefreshJobEntity> refreshJobEntity = persistenceService.fetchRefreshJobById(jobId);
-        if (refreshJobEntity.isPresent()) {
-            persistenceService.deleteRefreshJob(refreshJobEntity.get());
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        refreshJobEntity.ifPresent(jobEntity -> persistenceService.deleteRefreshJob(jobEntity));
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    private static RefreshJob convertRefreshJob(Object from) {
+        return MAPPER.convertValue(from, new TypeReference<>() {});
     }
 
 }
--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/controller/testingsupport/RefreshJobsControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/controller/testingsupport/RefreshJobsControllerTest.java
@@ -2,25 +2,36 @@ package uk.gov.hmcts.reform.orgrolemapping.controller.testingsupport;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.reform.orgrolemapping.controller.testingsupport.domain.RefreshJob;
 import uk.gov.hmcts.reform.orgrolemapping.data.RefreshJobEntity;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.UserRequest;
 import uk.gov.hmcts.reform.orgrolemapping.domain.service.PersistenceService;
 import uk.gov.hmcts.reform.orgrolemapping.helper.TestDataBuilder;
 
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 
 class RefreshJobsControllerTest {
     @Mock
     private PersistenceService persistenceService;
+
     @InjectMocks
     private final RefreshJobsController refreshJobsController = new RefreshJobsController(persistenceService);
 
@@ -31,46 +42,110 @@ class RefreshJobsControllerTest {
 
     @Test
     void insertJobByIdamIds() {
-        UserRequest userRequest = TestDataBuilder.buildUserRequest();
+
+        // GIVEN
         RefreshJobEntity refreshJobEntity = new RefreshJobEntity();
-        refreshJobEntity.setJobId(1L);
+        // fully populate entity: so we have at least one test to fully validate output values using assertRefreshJob
+        refreshJobEntity.setJobId(2L);
+        refreshJobEntity.setRoleCategory("test role category");
+        refreshJobEntity.setJurisdiction("test jurisdiction");
+        refreshJobEntity.setStatus("test status");
+        refreshJobEntity.setComments("test comments");
+        refreshJobEntity.setUserIds(new String[]{"test user id 1", "test user id 1"});
+        refreshJobEntity.setLog("test log");
+        refreshJobEntity.setLinkedJobId(1L);
+        refreshJobEntity.setCreated(ZonedDateTime.now());
         Mockito.when(persistenceService.persistRefreshJob(any()))
                 .thenReturn(refreshJobEntity);
-        refreshJobsController.insertJob("LEGAL_OPERATIONS",
-                                        "PUBLICLAW",
+        UserRequest userRequest = TestDataBuilder.buildUserRequest();
+
+        // WHEN
+        ResponseEntity<RefreshJob> response = refreshJobsController.insertJob("LEGAL_OPERATIONS",
+                "PUBLICLAW",
                 true,
-                1L,
-                "NEW",
+                null, // to be defaulted
+                null, // to be defaulted
                 "comments",
                 userRequest);
-        Mockito.verify(persistenceService, Mockito.times(2))
-                .persistRefreshJob(any());
+
+        // THEN
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertRefreshJob(refreshJobEntity, response.getBody());
+
+        ArgumentCaptor<RefreshJobEntity> refreshJobCaptor = ArgumentCaptor.forClass(RefreshJobEntity.class);
+        // NB: persistRefreshJob is called twice: once to create job ID and once to set job ID
+        Mockito.verify(persistenceService, Mockito.times(2)).persistRefreshJob(refreshJobCaptor.capture());
+        List<RefreshJobEntity> refreshJobValues = refreshJobCaptor.getAllValues();
+        // verify values in first save
+        RefreshJobEntity refreshJob1 = refreshJobValues.get(0);
+        assertEquals("LEGAL_OPERATIONS", refreshJob1.getRoleCategory());
+        assertEquals("PUBLICLAW", refreshJob1.getJurisdiction());
+        assertNull(refreshJob1.getLinkedJobId()); // NB: to be defaulted in save number 2
+        assertEquals("NEW", refreshJob1.getStatus()); // NB: defaulted
+        assertEquals("comments", refreshJob1.getComments());
+        assertEquals(userRequest.getUserIds().size(), refreshJob1.getUserIds().length);
+        assertNotNull(refreshJob1.getCreated()); // NB: defaulted as not in request
+        // verify second save has updated linked Job ID to match job ID from output of first save
+        RefreshJobEntity refreshJob2 = refreshJobValues.get(1);
+        assertEquals(refreshJobEntity.getJobId(), refreshJob2.getLinkedJobId());
     }
 
     @Test
     void insertJobForService() {
-        UserRequest userRequest = TestDataBuilder.buildUserRequest();
+
+        // GIVEN
         RefreshJobEntity refreshJobEntity = new RefreshJobEntity();
         refreshJobEntity.setJobId(1L);
-        refreshJobsController.insertJob("LEGAL_OPERATIONS",
+        Mockito.when(persistenceService.persistRefreshJob(any()))
+                .thenReturn(refreshJobEntity);
+
+        // WHEN
+        ResponseEntity<RefreshJob> response = refreshJobsController.insertJob("LEGAL_OPERATIONS",
                 "PUBLICLAW",
                 false,
                 1L,
                 "NEW",
                 "comments",
-                userRequest);
-        Mockito.verify(persistenceService, Mockito.times(1))
-                .persistRefreshJob(any());
+                null);
+
+        // THEN
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertRefreshJob(refreshJobEntity, response.getBody());
+        Mockito.verify(persistenceService, Mockito.times(1)).persistRefreshJob(any());
     }
 
     @Test
     void fetchJob() {
-        RefreshJobEntity refreshJobEntity = new RefreshJobEntity();
-        ResponseEntity<Object> response = ResponseEntity.status(HttpStatus.OK).body(refreshJobEntity);
 
+        // GIVEN
+        RefreshJobEntity refreshJobEntity = new RefreshJobEntity();
         Mockito.when(persistenceService.fetchRefreshJobById(1L))
                 .thenReturn(Optional.of(refreshJobEntity));
-        assertEquals(response, refreshJobsController.fetchJob(1L));
+
+        // WHEN
+        ResponseEntity<RefreshJob> response = refreshJobsController.fetchJob(1L);
+
+        // THEN
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(refreshJobEntity.getJobId(), response.getBody().getJobId());
+    }
+
+    @Test
+    void fetchJob_whenMissingJob() {
+
+        // GIVEN
+        RefreshJobEntity refreshJobEntity = new RefreshJobEntity();
+        Mockito.when(persistenceService.fetchRefreshJobById(1L))
+                .thenReturn(Optional.empty());
+
+        // WHEN
+        ResponseEntity<RefreshJob> response = refreshJobsController.fetchJob(1L);
+
+        // THEN
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
@@ -80,9 +155,50 @@ class RefreshJobsControllerTest {
         Mockito.when(persistenceService.fetchRefreshJobById(1L))
                 .thenReturn(Optional.of(refreshJobEntity));
 
-        refreshJobsController.removeJob(1L);
-        Mockito.verify(persistenceService,Mockito.times(1))
-                .deleteRefreshJob(refreshJobEntity);
+        // WHEN
+        ResponseEntity<Void> response = refreshJobsController.removeJob(1L);
+
+        // THEN
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        Mockito.verify(persistenceService,Mockito.times(1)).deleteRefreshJob(refreshJobEntity);
+    }
+
+    @Test
+    void removeJob_whenMissingJob() {
+        RefreshJobEntity refreshJobEntity = new RefreshJobEntity();
+
+        Mockito.when(persistenceService.fetchRefreshJobById(1L))
+                .thenReturn(Optional.empty());
+
+        // WHEN
+        ResponseEntity<Void> response = refreshJobsController.removeJob(1L);
+
+        // THEN
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        // NB: delete skipped but response still the same
+        Mockito.verify(persistenceService,Mockito.never()).deleteRefreshJob(refreshJobEntity);
+    }
+
+    private void assertRefreshJob(RefreshJobEntity expectedRJEntity, RefreshJob actualRefreshJob) {
+        assertEquals(expectedRJEntity.getJobId(), actualRefreshJob.getJobId());
+        assertEquals(expectedRJEntity.getRoleCategory(), actualRefreshJob.getRoleCategory());
+        assertEquals(expectedRJEntity.getJurisdiction(), actualRefreshJob.getJurisdiction());
+        assertEquals(expectedRJEntity.getStatus(), actualRefreshJob.getStatus());
+        assertEquals(expectedRJEntity.getComments(), actualRefreshJob.getComments());
+        if (expectedRJEntity.getUserIds() == null) {
+            assertNull(actualRefreshJob.getUserIds());
+        } else {
+            assertEquals(expectedRJEntity.getUserIds().length, actualRefreshJob.getUserIds().length);
+            assertThat(Arrays.stream(expectedRJEntity.getUserIds()).toList(),
+                    containsInAnyOrder(actualRefreshJob.getUserIds()));
+        }
+        assertEquals(expectedRJEntity.getLog(), actualRefreshJob.getLog());
+        assertEquals(expectedRJEntity.getLinkedJobId(), actualRefreshJob.getLinkedJobId());
+        if (expectedRJEntity.getCreated() == null) {
+            assertNull(actualRefreshJob.getCreated());
+        } else {
+            assertTrue(expectedRJEntity.getCreated().isEqual(actualRefreshJob.getCreated()));
+        }
     }
 
 }
