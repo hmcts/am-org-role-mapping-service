587e7e8f8c5b87387ffeea8704c9b6ad05e08ed0
Mike Brown <michael.brown@hmcts.net>
2024-06-26 16:33:15 +0100

    DTSAM-344 Spring Boot 3 Upgrade RARB - PLSql Flyway DB migration added to backup the old v4 tables, migration file renamed

---
+++ src/main/resources/db/migration/V20240619_344__DTSAM-344_SB3_RARB_tables_backup.sql
@@ -0,0 +1,77 @@
+-- DTSAM-344 Spring Boot 3 Upgrade RARB - spring-batch tables backed up and appended with v4 for rollback
+
+DO $$
+    BEGIN
+        IF EXISTS
+            ( SELECT 1
+              FROM   information_schema.tables
+              WHERE  table_schema = 'public'
+              AND    table_name = 'batch_job_execution_context'
+            )
+        THEN
+            create table batch_job_execution_context_v4 (like batch_job_execution_context including all);
+            insert into batch_job_execution_context_v4 select * from batch_job_execution_context;
+            drop table batch_job_execution_context;
+        END IF ;
+
+        IF EXISTS
+            ( SELECT 1
+              FROM   information_schema.tables
+              WHERE  table_schema = 'public'
+              AND    table_name = 'batch_step_execution_context'
+            )
+        THEN
+            create table batch_step_execution_context_v4 (like batch_step_execution_context including all);
+            insert into batch_step_execution_context_v4 select * from batch_step_execution_context;
+            drop table batch_step_execution_context;
+        END IF ;
+
+        IF EXISTS
+            ( SELECT 1
+              FROM   information_schema.tables
+              WHERE  table_schema = 'public'
+              AND    table_name = 'batch_step_execution'
+            )
+        THEN
+            create table batch_step_execution_v4 (like batch_step_execution including all);
+            insert into batch_step_execution_v4 select * from batch_step_execution;
+            drop table batch_step_execution;
+        END IF ;
+
+        IF EXISTS
+            ( SELECT 1
+              FROM   information_schema.tables
+              WHERE  table_schema = 'public'
+              AND    table_name = 'batch_job_execution_params'
+            )
+        THEN
+            create table batch_job_execution_params_v4 (like batch_job_execution_params including all);
+            insert into batch_job_execution_params_v4 select * from batch_job_execution_params;
+            drop table batch_job_execution_params;
+        END IF ;
+
+        IF EXISTS
+            ( SELECT 1
+              FROM   information_schema.tables
+              WHERE  table_schema = 'public'
+              AND    table_name = 'batch_job_execution'
+            )
+        THEN
+            create table batch_job_execution_v4 (like batch_job_execution including all);
+            insert into batch_job_execution_v4 select * from batch_job_execution;
+            drop table batch_job_execution;
+        END IF ;
+
+        IF EXISTS
+            ( SELECT 1
+              FROM   information_schema.tables
+              WHERE  table_schema = 'public'
+              AND    table_name = 'batch_job_instance'
+            )
+        THEN
+            create table batch_job_instance_v4 (like batch_job_instance including all);
+            insert into batch_job_instance_v4 select * from batch_job_instance;
+            drop table batch_job_instance;
+        END IF ;
+    END
+$$ ;
--- src/main/resources/db/migration/V20240619_344__DTSAM-344_SB3_RARB_tables_renamed.sql
+++
@@ -1,7 +0,0 @@
--- DTSAM-344 Spring Boot 3 Upgrade RARB - spring-batch tables appended with v4 for rollback
-alter table batch_job_execution_context rename to batch_job_execution_context_v4;
-alter table batch_step_execution_context rename to batch_step_execution_context_v4;
-alter table batch_step_execution rename to batch_step_execution_v4;
-alter table batch_job_execution_params rename to batch_job_execution_params_v4;
-alter table batch_job_execution rename to batch_job_execution_v4;
-alter table batch_job_instance rename to batch_job_instance_v4;
