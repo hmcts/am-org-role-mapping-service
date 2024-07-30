b8516079264dda513e55586c46fff4d98f744a54
Abul Syed <abul.syed@HMCTS.net>
2024-07-30 09:13:23 +0100

    DTSAM-434 update flyway

--- src/main/resources/db/migration/V20240726_434__DTSAM-434_PUBLICLAW_WA_1_4_base_flag_addition.sql
+++ src/main/resources/db/migration/V20240726_434__DTSAM-434_PUBLICLAW_WA_1_4_base_flag_addition.sql
@@ -1,8 +1,8 @@
 -- DTSAM-434: insert publiclaw_wa_1_4 base flag into flag_config table
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'local', 'employment', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'pr', 'employment', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'aat', 'employment', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'demo', 'employment', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'perftest', 'employment', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'ithc', 'employment', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'prod', 'employment', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'local', 'publiclaw', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'pr', 'publiclaw', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'aat', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'demo', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'perftest', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'ithc', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_4', 'prod', 'publiclaw', 'false');
