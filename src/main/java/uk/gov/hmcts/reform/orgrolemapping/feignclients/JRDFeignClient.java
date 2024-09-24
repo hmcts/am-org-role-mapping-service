58c3e6eb6a82caf3db9463102ff7ba958d071ec7
Matt Nayler <matt.nayler@cgi.com>
2024-09-24 10:08:27 +0100

    DTSAM-530 Adjust JRD Feign Client pagination
       [DTSAM-530](https://tools.hmcts.net/jira/browse/DTSAM-530)
    
    Adjust the pagination parameters in the JRD Feign Client.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/feignclients/JRDFeignClient.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/feignclients/JRDFeignClient.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.JRDUserRequest;
 import uk.gov.hmcts.reform.orgrolemapping.feignclients.configuration.FeignClientConfiguration;
 import uk.gov.hmcts.reform.orgrolemapping.feignclients.configuration.JRDFeignClientFallback;
@@ -30,8 +28,9 @@ public interface JRDFeignClient {
     @PostMapping(value = "/refdata/judicial/users")
     <T> ResponseEntity<List<T>> getJudicialDetailsByServiceName(
             @RequestBody JRDUserRequest userRequest,
-            @RequestParam(value = "page_size", required = false) Integer pageSize,
-            @RequestParam(value = "page_number", required = false) Integer pageNumber,
-            @RequestParam(value = "sort_direction", required = false) String sortDirection,
-            @RequestParam(value = "sort_column", required = false) String sortColumn);
+            @RequestHeader(value = "page_size", required = false) Integer pageSize,
+            @RequestHeader(value = "page_number", required = false) Integer pageNumber,
+            @RequestHeader(value = "sort_direction", required = false) String sortDirection,
+            @RequestHeader(value = "sort_column", required = false) String sortColumn);
+
 }
