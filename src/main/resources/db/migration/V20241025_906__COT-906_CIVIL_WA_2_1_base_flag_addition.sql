56e7e2f7e7a7c5b5ffa33fbacbc69284a4f4580a
Ben James <133333764+ben-james-hmcts@users.noreply.github.com>
2024-10-25 16:40:21 +0100

    Update src/main/resources/db/migration/V20241025_906__COT-906_CIVIL_WA_2_1_base_flag_addition.sql
    Co-authored-by: abul-syed <130474274+abul-syed@users.noreply.github.com>

--- src/main/resources/db/migration/V20241025_906__COT-906_CIVIL_WA_2_1_base_flag_addition.sql
+++ src/main/resources/db/migration/V20241025_906__COT-906_CIVIL_WA_2_1_base_flag_addition.sql
@@ -1,4 +1,4 @@
--- DTSAM-360: insert civil_wa_1_6 base flag into flag_config table
+-- COT-906: insert civil_wa_2_1 base flag into flag_config table
 INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('civil_wa_2_1', 'local', 'civil', 'false');
 INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('civil_wa_2_1', 'pr', 'civil', 'false');
 INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('civil_wa_2_1', 'aat', 'civil', 'false');
