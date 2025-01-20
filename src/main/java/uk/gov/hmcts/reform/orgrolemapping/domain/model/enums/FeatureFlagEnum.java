3ba37fe6cead50f62e145e5753a38f69ab9c9861
Ben James <ben.james@hmcts.net>
2025-01-20 14:36:16 +0000

    Added new flag for ST CIC changes

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/FeatureFlagEnum.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/FeatureFlagEnum.java
@@ -24,6 +24,7 @@ public enum FeatureFlagEnum {
     PRIVATELAW_WA_1_2("privatelaw_wa_1_2"),
     ST_CIC_WA_1_0("st_cic_wa_1_0"),
     ST_CIC_WA_1_1("st_cic_wa_1_1"),
+    ST_CIC_WA_1_2("st_cic_wa_1_2"),
     PUBLICLAW_WA_1_1("publiclaw_wa_1_1"),
     CIVIL_WA_1_2("civil_wa_1_2"),
     CIVIL_WA_1_3("civil_wa_1_3"),
