32e5c3ea68dd6b4f0503e060a9ce8e00a2270444
prashanthnerligeb <prashanth.nerlige@hmcts.net>
2025-03-05 15:26:14 +0000

    DTSAM-149 Enable 'iac_jrd_1_1' ORM flag in PROD addressed review comment

--- src/main/resources/db/migration/V20250305_149__DTSAM-149_Enable_iac_jrd_1_1_Prod.sql
+++ src/main/resources/db/migration/V20250305_149__DTSAM-149_Enable_iac_jrd_1_1_Prod.sql
@@ -1,2 +1,2 @@
--- enable civil_wa_2_1 flag in Prod for: COT-918 / COT-906
+-- enable iac_jrd_1_1 flag in Prod for: DTSAM-149 / DTSAM-96
 update flag_config set status='true' where flag_name='iac_jrd_1_1' and env in ('demo', 'aat', 'perftest', 'ithc', 'prod');
