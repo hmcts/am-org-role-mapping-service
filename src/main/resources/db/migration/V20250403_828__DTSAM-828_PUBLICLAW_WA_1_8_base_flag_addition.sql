9277f2d69a7bf106d8b002e93b560a1e5820646b
Matt Nayler <matt.nayler@cgi.com>
2025-04-03 13:39:41 +0100

    DTSAM-828 Adjust flag creation flyway script.

--- src/main/resources/db/migration/V20250403_828__DTSAM-828_PUBLICLAW_WA_1_8_base_flag_addition.sql
+++ src/main/resources/db/migration/V20250403_828__DTSAM-828_PUBLICLAW_WA_1_8_base_flag_addition.sql
@@ -1,8 +1,8 @@
 -- DTSAM-828: insert publiclaw_wa_1_8 base flag into flag_config table
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'local', 'privatelaw', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'pr', 'privatelaw', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'aat', 'privatelaw', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'demo', 'privatelaw', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'perftest', 'privatelaw', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'ithc', 'privatelaw', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'prod', 'privatelaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'local', 'publiclaw', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'pr', 'publiclaw', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'aat', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'demo', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'perftest', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'ithc', 'publiclaw', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('publiclaw_wa_1_8', 'prod', 'publiclaw', 'false');
