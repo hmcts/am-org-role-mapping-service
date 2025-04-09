a8c627fc3afddb1ed2b04ff75d08b636dc5700a5
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2025-04-09 12:32:00 +0100

    DTSAM-814 Remove unused import.

--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/OrganisationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/OrganisationServiceTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.orgrolemapping.domain.service;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
