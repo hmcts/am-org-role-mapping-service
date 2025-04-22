e5ecda4930ed76e0c676882ee42cf520fa9e29c2
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2025-04-22 13:45:18 +0100

    DTSAM-814 Correct FeignClient to use system user.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/feignclients/PRDFeignClient.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/feignclients/PRDFeignClient.java
@@ -12,9 +12,10 @@ import uk.gov.hmcts.reform.orgrolemapping.domain.model.OrganisationByProfileIdsR
 import uk.gov.hmcts.reform.orgrolemapping.feignclients.configuration.FeignClientConfiguration;
 import uk.gov.hmcts.reform.orgrolemapping.feignclients.configuration.FeignClientInterceptor;
 import uk.gov.hmcts.reform.orgrolemapping.feignclients.configuration.PRDFeignClientFallback;
+import uk.gov.hmcts.reform.orgrolemapping.feignclients.configuration.RdFeignClientInterceptor;
 
 @FeignClient(value = "prdClient", url = "${feign.client.config.prdClient.url}",
-        configuration = {FeignClientConfiguration.class, FeignClientInterceptor.class},
+        configuration = {FeignClientConfiguration.class, RdFeignClientInterceptor.class},
         fallback = PRDFeignClientFallback.class
 )
 public interface PRDFeignClient {
