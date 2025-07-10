783870f1e9370be28cdd191e26c2fafb6ba3a64e
Matt Nayler <57350764+mattnayler@users.noreply.github.com>
2025-07-10 09:58:28 +0100

    DTSAM-931 Adjust PRD Pact config (#2380)
    * DTSAM-931 Adjust PRD Pact config
    
    * DTSAM-931 Adjust PRD Pact config

--- src/contractTest/java/uk/gov/hmcts/reform/refdata/RefDataProfessionalUserInternalConsumerTestV2.java
+++ src/contractTest/java/uk/gov/hmcts/reform/refdata/RefDataProfessionalUserInternalConsumerTestV2.java
@@ -37,8 +37,11 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @PactFolder("pacts")
 @PactTestFor(providerName = "referenceData_professionalInternalUsersV2", port = "8090")
-@ContextConfiguration(classes = {RefDataCaseworkerConsumerApplication.class})
-@TestPropertySource(properties = {"feign.client.config.prdClient.url=http://localhost:8090"})
+@ContextConfiguration(classes = {RefDataConsumerApplication.class})
+@TestPropertySource(properties = {
+    "feign.client.config.crdclient.url=http://localhost:8991",
+    "feign.client.config.prdClient.url=http://localhost:8090"
+})
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class RefDataProfessionalUserInternalConsumerTestV2 {
 
