f0062bc9af53509ae211f6d014ab258dd5266024
Matt Nayler <57350764+mattnayler@users.noreply.github.com>
2025-07-30 11:30:20 +0100

    DTSAM-881 Enable 'privatelaw_wa_1_7' ORM flag in PROD

--- src/main/resources/db/migration/V20250730_881__DTSAM-881_Enable_privatelaw_wa_1_7_Prod.sql
+++ src/main/resources/db/migration/V20250730_881__DTSAM-881_Enable_privatelaw_wa_1_7_Prod.sql
@@ -1,2 +1,2 @@
--- enable privatelaw_wa_1_7 flag in Prod for: DTSAM-881 / DTSAM-860
+-- enable privatelaw_wa_1_7 flag in Prod for: DTSAM-881 / DTSAM-860 
 update flag_config set status='true' where flag_name='privatelaw_wa_1_7' and env in ('demo', 'aat', 'perftest', 'ithc', 'prod');
