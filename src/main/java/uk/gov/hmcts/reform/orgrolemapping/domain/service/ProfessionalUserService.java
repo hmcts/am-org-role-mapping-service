7f1ad82e38577b3370f3dd5aa79298c5651a767e
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2025-08-01 10:00:16 +0100

    DTSAM-856 Add brackets for success condition.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/ProfessionalUserService.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/ProfessionalUserService.java
@@ -329,7 +329,7 @@ public class ProfessionalUserService {
         }
 
         markProcessStatus(processMonitorDto,
-            successfulJobCount > 0 || successfulJobCount == 0 && failedJobCount == 0,
+            successfulJobCount > 0 || (successfulJobCount == 0 && failedJobCount == 0),
             failedJobCount > 0,
             errorMessageBuilder.toString());
         processEventTracker.trackEventCompleted(processMonitorDto);
