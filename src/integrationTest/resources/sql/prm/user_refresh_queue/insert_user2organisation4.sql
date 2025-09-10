a3f6b754f949d79bc36a3469999eb3b15a98f18f
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2025-09-10 11:45:50 +0100

    DTSAM-914 Realign the access types version to 1.

--- src/integrationTest/resources/sql/prm/user_refresh_queue/insert_user2organisation4.sql
+++ src/integrationTest/resources/sql/prm/user_refresh_queue/insert_user2organisation4.sql
@@ -1,3 +1,3 @@
 INSERT INTO user_refresh_queue (user_id, organisation_id, access_types_min_version, active, organisation_profile_ids, organisation_status, last_updated, user_last_updated, access_types)
-SELECT 'user2', '4', 2, true, '{SOLICITOR_PROFILE}', 'ACTIVE', '2020-01-01T13:20:01.046Z', '2020-01-01T13:30:01.046Z',
+SELECT 'user2', '4', 1, true, '{SOLICITOR_PROFILE}', 'ACTIVE', '2020-01-01T13:20:01.046Z', '2020-01-01T13:30:01.046Z',
 '{"jurisdictionId": "CIVIL", "organisationProfileId": "SOLICITOR_PROFILE", "accessTypeId": "civil-cases-1","enabled": true}';
