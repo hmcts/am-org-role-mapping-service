0e5615ad4a52425268515e224aeac4606797f940
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2025-10-10 08:04:01 +0100

    DTSAM-202 Remove unused security mocks.

--- src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/scheduler/PrmSchedulerCleanupIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/scheduler/PrmSchedulerCleanupIntegrationTest.java
@@ -1,23 +1,15 @@
 package uk.gov.hmcts.reform.orgrolemapping.scheduler;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.jdbc.Sql;
 import uk.gov.hmcts.reform.orgrolemapping.controller.BaseTestIntegration;
-import uk.gov.hmcts.reform.orgrolemapping.controller.utils.MockUtils;
 import uk.gov.hmcts.reform.orgrolemapping.controller.utils.WiremockFixtures;
 import uk.gov.hmcts.reform.orgrolemapping.data.OrganisationRefreshQueueRepository;
 import uk.gov.hmcts.reform.orgrolemapping.data.UserRefreshQueueRepository;
 import uk.gov.hmcts.reform.orgrolemapping.monitoring.models.ProcessMonitorDto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.doReturn;
 
 class PrmSchedulerCleanupIntegrationTest extends BaseTestIntegration {
 
@@ -29,26 +21,10 @@ class PrmSchedulerCleanupIntegrationTest extends BaseTestIntegration {
 
     private final WiremockFixtures wiremockFixtures = new WiremockFixtures();
 
-    @Mock
-    private Authentication authentication;
-
-    @Mock
-    private SecurityContext securityContext;
-
     private ProcessMonitorDto processMonitorDto;
 
     @Autowired
     private Scheduler scheduler;
-
-    @BeforeEach
-    void setUp() throws JsonProcessingException {
-        doReturn(authentication).when(securityContext).getAuthentication();
-        SecurityContextHolder.setContext(securityContext);
-        MockUtils.setSecurityAuthorities(authentication, MockUtils.ROLE_CASEWORKER);
-        wiremockFixtures.resetRequests();
-        wiremockFixtures.stubIdamCall();
-        processMonitorDto = new ProcessMonitorDto("PRM Process 6 - Refresh users - Batch mode [Test]");
-    }
 
     //# UserRefreshQueue cleanup tests
 
