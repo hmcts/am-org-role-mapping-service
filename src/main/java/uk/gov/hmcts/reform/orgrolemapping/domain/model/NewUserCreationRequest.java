e234e2a4c82c0fd387ee33a6bd1b57cdd81aca87
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2025-10-14 12:05:45 +0100

    Adding domain objects

---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/NewUserCreationRequest.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.reform.orgrolemapping.domain.model;
+
+import java.util.List;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+
+@Getter
+@Setter
+@Builder(builderMethodName = "aNewUserCreationRequest")
+public class NewUserCreationRequest {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<String> roles;
+    private boolean resendInvite;
+    private Set<UserAccessType> userAccessTypes;
+
+    @JsonCreator
+    public NewUserCreationRequest(
+            @JsonProperty("firstName") String firstName,
+            @JsonProperty("lastName") String lastName,
+            @JsonProperty("email") String emailAddress,
+            @JsonProperty("roles") List<String> roles,
+            @JsonProperty("resendInvite") Boolean resendInvite,
+            @JsonProperty("userAccessTypes") Set<UserAccessType> userAccessTypes
+    ) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = StringUtils.isBlank(emailAddress) ? emailAddress : emailAddress.toLowerCase();
+        this.roles = roles;
+        this.resendInvite = resendInvite;
+        this.userAccessTypes = userAccessTypes;
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/UserProfileUpdatedData.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.reform.orgrolemapping.domain.model;
+
+import java.util.Set;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserProfileUpdatedData {
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String idamStatus;
+
+    private Set<RoleName> rolesAdd;
+
+    private Set<RoleName> rolesDelete;
+
+    @JsonInclude(NON_NULL)
+    private Set<UserAccessType> userAccessTypes;
+
+    @JsonCreator
+    public UserProfileUpdatedData(@JsonProperty(value = "email") String email,
+                                  @JsonProperty(value = "firstName") String firstName,
+                                  @JsonProperty(value = "lastName") String lastName,
+                                  @JsonProperty(value = "idamStatus") String idamStatus,
+                                  @JsonProperty(value = "rolesAdd") Set<RoleName> rolesAdd,
+                                  @JsonProperty(value = "rolesDelete") Set<RoleName> rolesDelete,
+                                  @JsonProperty(value = "userAccessTypes") Set<UserAccessType> userAccessTypes
+    ) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idamStatus = idamStatus;
+        this.rolesAdd = rolesAdd;
+        this.rolesDelete = rolesDelete;
+        this.userAccessTypes = userAccessTypes;
+    }
+
+}
