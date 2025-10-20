75b324acbae0da519899d031d9b31e4a1e33b3f0
abul-syed <130474274+abul-syed@users.noreply.github.com>
2025-10-20 11:26:45 +0100

    DTSAM-367 update SSCS mappings for case-allocator & task-supervisor (#1989)
    * DTSAM-367 update sscs role mappings
    
    * DTSAM-367 conditionally adding region to CA/TS when required
    
    * DTSAM-367 update FTAs
    
    * DTSAM-367 rm scenarios already present in 3 tests below
    
    * DTSAM-367 FTA reconfigure in advance of merge conflicts
    
    * DTSAM-367 Unit test fix after merge conflicts (#2513)
    
    ---------
    
    Co-authored-by: Matt Nayler <57350764+mattnayler@users.noreply.github.com>
    Co-authored-by: mikebrownccd <104495891+mikebrownccd@users.noreply.github.com>
    Co-authored-by: Matt Nayler <matt.nayler@cgi.com>

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/FeatureFlagEnum.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/FeatureFlagEnum.java
@@ -9,6 +9,7 @@ public enum FeatureFlagEnum {
     SSCS_WA_1_1("sscs_wa_1_1"),
     SSCS_WA_1_2("sscs_wa_1_2"),
     SSCS_WA_1_3("sscs_wa_1_3"),
+    SSCS_WA_1_4("sscs_wa_1_4"),
     SSCS_WA_1_5("sscs_wa_1_5"),
     CIVIL_WA_1_0("civil_wa_1_0"),
     PRIVATELAW_HEARING_1_0("privatelaw_hearing_1_0"),
