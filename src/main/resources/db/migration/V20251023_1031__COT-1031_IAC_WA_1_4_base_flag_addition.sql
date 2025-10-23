55577eb5a8da484ea234997e8716e9d19f01e4b0
Ben James <ben.james@hmcts.net>
2025-10-23 15:49:44 +0100

    Updated service name value in migration file

--- src/main/resources/db/migration/V20251023_1031__COT-1031_IAC_WA_1_4_base_flag_addition.sql
+++ src/main/resources/db/migration/V20251023_1031__COT-1031_IAC_WA_1_4_base_flag_addition.sql
@@ -1,8 +1,8 @@
 -- COT-1031: insert iac_wa_1_4 base flag into flag_config table
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'local', 'civil', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'pr', 'civil', 'true');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'aat', 'civil', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'demo', 'civil', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'perftest', 'civil', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'ithc', 'civil', 'false');
-INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'prod', 'civil', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'local', 'iac', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'pr', 'iac', 'true');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'aat', 'iac', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'demo', 'iac', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'perftest', 'iac', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'ithc', 'iac', 'false');
+INSERT INTO flag_config (flag_name, env, service_name, status) VALUES ('iac_wa_1_4', 'prod', 'iac', 'false');
