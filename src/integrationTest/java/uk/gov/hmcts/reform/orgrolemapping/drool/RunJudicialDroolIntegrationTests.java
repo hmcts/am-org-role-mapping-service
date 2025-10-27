45aeafe97f61ed7207d7b90f793fc2997b9392d1
Matt Nayler <matt.nayler@cgi.com>
2025-10-27 14:15:21 +0000

    DTSAM-642 refactor PublicLaw test arguments into the new format

--- src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/drool/RunJudicialDroolIntegrationTests.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/drool/RunJudicialDroolIntegrationTests.java
@@ -44,6 +44,8 @@ public class RunJudicialDroolIntegrationTests extends BaseDroolTestIntegration {
 
         arguments.addAll(BasicJudicialIT.getTestArguments());
 
+        arguments.addAll(PublicLawJudicialIT.getTestArguments());
+
         return arguments.stream()
             .map(DroolJudicialTestArguments::toArguments);
     }
