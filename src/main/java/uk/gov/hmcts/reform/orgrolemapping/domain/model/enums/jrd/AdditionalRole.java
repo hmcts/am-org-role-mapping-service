4efd34db2910c40d0c8e685b91cd646ac6425c2e
Matt Nayler <57350764+mattnayler@users.noreply.github.com>
2025-11-04 09:18:03 +0000

    DTSAM-1037  Judicial utility functions (#2478)
    * DTSAM-1037 Utility functions to check appointmentType
    
    * DTSAM-1037 Utility functions to check ValidEndDate
    
    * DTSAM-1037 Utility functions to check ValidAuthorisation
    
    * DTSAM-1037 Constants for use in judicial drool rules
    
    * DTSAM-1037 Utility functions to check AppointmentCode
    
    * DTSAM-970 Adjust ET Drool unit tests in prepartion of move to use Appointment codes
    
    * DTSAM-1037 Utility functions to check AppointmentCode (code value as String)
    
    * DTSAM-970 Adjust ET Drool unit tests in prepartion of move to use Appointment codes
    
    * DTSAM-1037 Utility functions to check AdditionalRole code
    
    * DTSAM-970 Adjust ET Drool unit tests in prepartion of move to use AdditionalRole codes
    
    * DTSAM-1037 Adjust constants used in judicial drool rules
    
    * DTSAM-1037 Adjust constants used in judicial drool rules
    
    * DTSAM-1037 Utility functions to check AppointmentCode (switch from roleId to appointmentCode)
    
    * Adjust jenkins artifact configuration
    
    * Adjust jenkins artifact configuration

---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/jrd/AdditionalRole.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.orgrolemapping.domain.model.enums.jrd;
+
+import java.util.List;
+
+public enum AdditionalRole implements AdditionalRoleEnum {
+
+    // NB: for codes see:
+    //  https://tools.hmcts.net/confluence/display/DTSAM/Judicial+Reconciliation#JudicialReconciliation-AdditionalRoleCodes
+
+    ACTING_REGIONAL_EMPLOYMENT_JUDGE("Acting Regional Employment Judge", List.of("90004"));
+
+    private final String name;
+    private final List<String> codes; // support for multiple codes in future (i.e. different spellings for same role)
+
+    AdditionalRole(String name, List<String> codes) {
+        this.name = name;
+        this.codes = codes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getCodes() {
+        return codes;
+    }
+
+}
---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/jrd/AppointmentGroup.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.reform.orgrolemapping.domain.model.enums.jrd;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum AppointmentGroup implements AppointmentEnum {
+
+    ANY_TRIBUNAL_MEMBER("Any Tribunal Member", List.of(
+        Appointment.TRIBUNAL_MEMBER,
+        Appointment.TRIBUNAL_MEMBER_DENTIST,
+        Appointment.TRIBUNAL_MEMBER_DISABILITY,
+        Appointment.TRIBUNAL_MEMBER_FARMER,
+        Appointment.TRIBUNAL_MEMBER_FINANCIALLY_QUALIFIED,
+        Appointment.TRIBUNAL_MEMBER_LANDOWNER,
+        Appointment.TRIBUNAL_MEMBER_LAY,
+        Appointment.TRIBUNAL_MEMBER_MEDICAL,
+        Appointment.TRIBUNAL_MEMBER_OPTOMETRIST,
+        Appointment.TRIBUNAL_MEMBER_PHARMACIST,
+        Appointment.TRIBUNAL_MEMBER_PROFESSIONAL,
+        Appointment.TRIBUNAL_MEMBER_SERVICE,
+        Appointment.TRIBUNAL_MEMBER_VALUER,
+        Appointment.TRIBUNAL_MEMBER_VALUER_CHAIRMAN
+    ));
+
+
+    private final String name;
+    private final List<String> codes;
+    private final List<Appointment> members;
+
+    AppointmentGroup(String name, List<Appointment> appointments) {
+        this.name = name;
+        this.codes = appointments.stream().flatMap(a -> a.getCodes().stream()).collect(Collectors.toList());
+        this.members = appointments;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getCodes() {
+        return codes;
+    }
+
+    public List<Appointment> getMembers() {
+        return members;
+    }
+
+}
