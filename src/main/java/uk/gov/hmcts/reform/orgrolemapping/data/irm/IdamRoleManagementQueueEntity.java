f0931688ae8afba45452e229c94c02de31d0dcbc
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-02-03 09:12:14 +0000

    DTSAM-1173 Add the idam_role_management_queue entity and repository.

---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/data/irm/IdamRoleManagementQueueEntity.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.reform.orgrolemapping.data.irm;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+
+@Builder(toBuilder = true)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "irm_queue")
+public class IdamRoleManagementQueueEntity {
+
+    @Id
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "user_type", nullable = false)
+    private String userType;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "data", nullable = false)
+    private String data;
+
+    @Column(name = "last_updated", nullable = false)
+    private LocalDateTime lastUpdated;
+
+    @Column(name = "active", nullable = false)
+    private Boolean active;
+
+    @Column(name = "retry")
+    private Integer retry;
+
+    @Column(name = "retry_after")
+    private LocalDateTime retryAfter;
+}
---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/data/irm/IdamRoleManagementQueueRepository.java
@@ -0,0 +1,65 @@
+package uk.gov.hmcts.reform.orgrolemapping.data.irm;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Propagation;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface IdamRoleManagementQueueRepository extends CrudRepository<IdamRoleManagementQueueEntity, Long> {
+
+    @Modifying
+    @Query(value = """
+        insert into idam_role_management_queue (user_id, userType, data, last_updated, active)
+        values (:userId, :userType, :data, :lastUpdated, true)
+        on conflict (user_id) do update
+        set last_updated = now(),
+            active = true,
+            retry = 0,
+            retry_after = now(),
+            user_type = excluded.user_type,
+            data = excluded.data
+        where excluded.last_updated > idam_role_management_queue.last_updated
+        """, nativeQuery = true)
+    void upsert(String userId, String userType, String data, LocalDateTime lastUpdated);
+
+    @Modifying
+    @Query(value = """
+        update idam_role_management_queue
+        set active = false,
+            retry = 0,
+            retry_after = now()
+        where user_id = :userId
+        and last_updated <= :lastUpdated
+        """, nativeQuery = true)
+    void clearRecord(String userId, LocalDateTime lastUpdated);
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Modifying
+    @Query(value = """
+        update idam_role_management_queue 
+        set retry = case 
+                when retry = 0 then 1 
+                when retry = 1 then 2 
+                when retry = 2 then 3 
+                when retry = 4 then 0 
+                else 4 
+            end, 
+            retry_after = case 
+                when retry = 0 then now() + (interval '1' Minute) * CAST(:retryOneIntervalMin AS INTEGER) 
+                when retry = 1 then now() + (interval '1' Minute) * CAST(:retryTwoIntervalMin AS INTEGER) 
+                when retry = 2 then now() + (interval '1' Minute) * CAST(:retryThreeIntervalMin AS INTEGER) 
+                when retry = 4 then now() 
+                else NULL 
+            end 
+        where user_id = :userId
+        """, nativeQuery = true)
+    void updateRetry(String userId, 
+                     String retryOneIntervalMin,
+                     String retryTwoIntervalMin, 
+                     String retryThreeIntervalMin);
+}
