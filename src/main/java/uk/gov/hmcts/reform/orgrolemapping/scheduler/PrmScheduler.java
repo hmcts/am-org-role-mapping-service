5c71f239e3e6f3bf138cb78bd559cf4b98abb0f0
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-02-17 14:53:38 +0000

    DTSAM-1175 Add the test of the scheduling to the branch.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/scheduler/PrmScheduler.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/scheduler/PrmScheduler.java
@@ -4,23 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.orgrolemapping.domain.service.IdamRoleMappingService;
 import uk.gov.hmcts.reform.orgrolemapping.monitoring.models.ProcessMonitorDto;
 
 @Slf4j
 @Service
-@ConditionalOnProperty(name = "idam.role.management.scheduling.enabled")
-public class IrmScheduler {
-
-    private final IdamRoleMappingService idamRoleMappingService;
-
-    public IrmScheduler(IdamRoleMappingService idamRoleMappingService) {
-        this.idamRoleMappingService = idamRoleMappingService;
-    }
+@ConditionalOnProperty(name = "professional.role.mapping.scheduling.enabled")
+public class PrmScheduler {
 
     @Scheduled(cron = "${idam.role.management.scheduling.judicial.cron}")
     public ProcessMonitorDto processJudicialQueue() {
-        log.info("Starting IRM Scheduler");
-        return idamRoleMappingService.processJudicialQueue();
+        log.info("Starting PRM Scheduler Test");
+        ProcessMonitorDto processMonitorDto = new ProcessMonitorDto("Test");
+        processMonitorDto.markAsSuccess();
+        return processMonitorDto;
     }
 }
