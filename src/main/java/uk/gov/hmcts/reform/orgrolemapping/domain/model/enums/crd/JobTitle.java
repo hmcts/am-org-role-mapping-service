d981a5b6dd460208874cc505006feaa9147f6f56
padmapriyanalam <padmapriya.nalam@gmail.com>
2026-03-17 16:55:39 +0000

    POFCC-101 - Judicial mapping role

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/crd/JobTitle.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/domain/model/enums/crd/JobTitle.java
@@ -25,7 +25,9 @@ public enum JobTitle {
     IBCS("IBCA Caseworker", "19"),
     WLU_ADMIN("WLU Administrator", "20"),
     WLU_TEAM_LEADER("WLU Team Leader", "21"),
-    HRS_TEAM_LEADER("HRS Team Leader", "22");
+    HRS_TEAM_LEADER("HRS Team Leader", "22"),
+    HMCTS_JUDICIARY("hmcts- Judiciary ", "23"),
+    FEE_PAID_JUDGE("fee-paid-judge", "24");
 
     private final String roleName;
     private final String roleId;
