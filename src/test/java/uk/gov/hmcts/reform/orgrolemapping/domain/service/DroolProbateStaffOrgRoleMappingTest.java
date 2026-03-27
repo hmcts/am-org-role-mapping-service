a096bd57c892c6b722b2f688a8a79c82f061d6f4
Ben James <ben.james@hmcts.net>
2026-03-27 13:12:52 +0000

    Updated tests

--- src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/DroolProbateStaffOrgRoleMappingTest.java
+++ src/test/java/uk/gov/hmcts/reform/orgrolemapping/domain/service/DroolProbateStaffOrgRoleMappingTest.java
@@ -35,130 +35,30 @@ class DroolProbateStaffOrgRoleMappingTest extends DroolBase {
     static Map<String, String> expectedRoleNameWorkTypesMap = new HashMap<>();
 
     static {
-        expectedRoleNameWorkTypesMap.put("hearing-centre-admin", "hearing_work, upper_tribunal, routine_work, "
-                + "review_case, bail_work, stf_24w_hearing_work, stf_24w_upper_tribunal, stf_24w_routine_work");
-        expectedRoleNameWorkTypesMap.put("national-business-centre", "hearing_work, upper_tribunal, routine_work, "
-                + "stf_24w_hearing_work, stf_24w_upper_tribunal, stf_24w_routine_work");
-        expectedRoleNameWorkTypesMap.put("ctsc", "hearing_work, upper_tribunal, routine_work, "
-                + "stf_24w_hearing_work, stf_24w_upper_tribunal, stf_24w_routine_work");
-        expectedRoleNameWorkTypesMap.put("ctsc-team-leader", "hearing_work, upper_tribunal, routine_work, "
-                + "stf_24w_hearing_work, stf_24w_upper_tribunal, stf_24w_routine_work");
-        expectedRoleNameWorkTypesMap.put("senior-tribunal-caseworker", "hearing_work, routine_work, "
-                + "decision_making_work, applications, stf_24w_hearing_work, stf_24w_routine_work, "
-                + "stf_24w_decision_making_work, stf_24w_applications");
-        expectedRoleNameWorkTypesMap.put("tribunal-caseworker", "hearing_work, routine_work, "
-                + "decision_making_work, applications, stf_24w_hearing_work, stf_24w_routine_work, "
-                + "stf_24w_decision_making_work, stf_24w_applications");
-        expectedRoleNameWorkTypesMap.put("hmcts-ctsc", null);
-        expectedRoleNameWorkTypesMap.put("hmcts-legal-operations", null);
-        expectedRoleNameWorkTypesMap.put("hmcts-admin", null);
-        expectedRoleNameWorkTypesMap.put("task-supervisor", null);
-        expectedRoleNameWorkTypesMap.put("case-allocator", null);
-    }
-
-
-    @ParameterizedTest
-    @CsvSource({
-        "3,BFA1,'hmcts-admin,hearing-centre-admin',N,N",
-        "3,BFA1,'hmcts-admin,hearing-centre-admin,task-supervisor',Y,N",
-        "3,BFA1,'hmcts-admin,hearing-centre-admin,case-allocator',N,Y",
-        "3,BFA1,'hmcts-admin,hearing-centre-admin,task-supervisor,case-allocator',Y,Y",
-        "3,XYZ1,'',Y,Y",
-
-        "4,BFA1,'hmcts-admin,hearing-centre-admin',N,N",
-        "4,BFA1,'hmcts-admin,hearing-centre-admin,task-supervisor',Y,N",
-        "4,BFA1,'hmcts-admin,hearing-centre-admin,case-allocator',N,Y",
-        "4,BFA1,'hmcts-admin,hearing-centre-admin,task-supervisor,case-allocator',Y,Y",
-        "4,XYZ1,'',Y,Y",
-
-        "5,BFA1,'hmcts-admin,hearing-centre-admin',N,N",
-        "5,BFA1,'hmcts-admin,hearing-centre-admin,task-supervisor',Y,N",
-        "5,BFA1,'hmcts-admin,hearing-centre-admin,case-allocator',N,Y",
-        "5,BFA1,'hmcts-admin,hearing-centre-admin,task-supervisor,case-allocator',Y,Y",
-        "5,XYZ1,'',Y,Y",
-
-        "6,BFA1,'hmcts-admin,national-business-centre',N,N",
-        "6,BFA1,'hmcts-admin,national-business-centre,task-supervisor',Y,N",
-        "6,BFA1,'hmcts-admin,national-business-centre,case-allocator',N,Y",
-        "6,BFA1,'hmcts-admin,national-business-centre,task-supervisor,case-allocator',Y,Y",
-        "6,XYZ1,'',Y,Y",
-
-        "7,BFA1,'hmcts-admin,national-business-centre',N,N",
-        "7,BFA1,'hmcts-admin,national-business-centre,task-supervisor',Y,N",
-        "7,BFA1,'hmcts-admin,national-business-centre,case-allocator',N,Y",
-        "7,BFA1,'hmcts-admin,national-business-centre,task-supervisor,case-allocator',Y,Y",
-        "7,XYZ1,'',Y,Y",
-
-        "8,BFA1,'hmcts-admin,national-business-centre',N,N",
-        "8,BFA1,'hmcts-admin,national-business-centre,task-supervisor',Y,N",
-        "8,BFA1,'hmcts-admin,national-business-centre,case-allocator',N,Y",
-        "8,BFA1,'hmcts-admin,national-business-centre,task-supervisor,case-allocator',Y,Y",
-        "8,XYZ1,'',Y,Y"
-    })
-    void shouldReturnIacAdminMappings(String roleId, String serviceCode, String expectedRoles,
-                                        String taskSupervisorFlag, String caseAllocatorFlag) {
-        allProfiles.clear();
-
-        CaseWorkerAccessProfile cap = UserAccessProfileBuilder.buildUserAccessProfileForRoleId2();
-        cap.setServiceCode(serviceCode);
-        cap.setSuspended(false);
-        cap.setRoleId(roleId);
-        cap.setTaskSupervisorFlag(taskSupervisorFlag);
-        cap.setCaseAllocatorFlag(caseAllocatorFlag);
-        allProfiles.add(cap);
-
-        //Execute Kie session
-        List<RoleAssignment> roleAssignments =
-                buildExecuteKieSession(getAllFeatureFlagsToggleByJurisdiction("PROBATE", true));
-
-        //assertion
-        String[] roleNames = StringUtils.isEmpty(expectedRoles) ? new String[0] : expectedRoles.split(",");
-        assertEquals(roleNames.length, roleAssignments.size());
-        assertThat(roleAssignments.stream().map(RoleAssignment::getRoleName).collect(Collectors.toList()),
-                containsInAnyOrder(roleNames));
-        roleAssignments.forEach(r -> {
-            assertEquals(RoleCategory.ADMIN, r.getRoleCategory());
-            assertEquals(RoleType.ORGANISATION, r.getRoleType());
-            assertEquals(cap.getId(), r.getActorId());
-        });
-
-        roleAssignments.forEach(r -> {
-            if (r.getRoleName().equals("hmcts-admin")) {
-                assertNull(r.getAttributes().get("jurisdiction"));
-                assertEquals(Classification.PRIVATE, r.getClassification());
-                assertEquals(GrantType.BASIC, r.getGrantType());
-            } else {
-                assertEquals("IA", r.getAttributes().get("jurisdiction").asText());
-                assertEquals(Classification.PUBLIC, r.getClassification());
-                assertEquals(GrantType.STANDARD, r.getGrantType());
-            }
-            assertEquals(cap.getPrimaryLocationId(), r.getAttributes().get("primaryLocation").asText());
-            if (expectedRoleNameWorkTypesMap.containsKey(r.getRoleName())) {
-                String expectedWorkTypes = expectedRoleNameWorkTypesMap.get(r.getRoleName());
-                String actualWorkTypes = null;
-                if (r.getAttributes().get("workTypes") != null) {
-                    actualWorkTypes = r.getAttributes().get("workTypes").asText();
-                }
-                assertEquals(expectedWorkTypes, actualWorkTypes);
-            } else {
-                assertFalse(r.getAttributes().containsKey("workTypes"));
-            }
-        });
+        expectedRoleNameWorkTypesMap.put("tribunal-caseworker", "decision_making_work");
+        expectedRoleNameWorkTypesMap.put("specific-access-approver-legal-ops", "access_requests");
+        expectedRoleNameWorkTypesMap.put("ctsc", "applications, review_case, stopped_applications, "
+                + "decision_making_work");
+        expectedRoleNameWorkTypesMap.put("specific-access-approver-judiciary", "access_requests");
+        expectedRoleNameWorkTypesMap.put("specific-access-approver-ctsc", "access_requests");
+        expectedRoleNameWorkTypesMap.put("specific-access-approver-admin", "access_requests");
     }
 
     @ParameterizedTest
     @CsvSource({
-        "9,BFA1,'hmcts-ctsc,ctsc,ctsc-team-leader',N,N",
-        "9,BFA1,'hmcts-ctsc,ctsc,ctsc-team-leader,task-supervisor',Y,N",
-        "9,BFA1,'hmcts-ctsc,ctsc,ctsc-team-leader,case-allocator',N,Y",
-        "9,BFA1,'hmcts-ctsc,ctsc,ctsc-team-leader,task-supervisor,case-allocator',Y,Y",
-        "9,XYZ1,'',Y,Y",
+        "9,ABA6,'ctsc-team-leader,ctsc,hmcts-ctsc,specific-access-approver-judiciary,specific-access-approver-ctsc,"
+                + "specific-access-approver-admin',N,N",
+        "9,ABA6,'ctsc-team-leader,ctsc,hmcts-ctsc,specific-access-approver-judiciary,specific-access-approver-ctsc,"
+                + "specific-access-approver-admin,task-supervisor',Y,N",
+        "9,ABA6,'ctsc-team-leader,ctsc,hmcts-ctsc,specific-access-approver-judiciary,specific-access-approver-ctsc,"
+                + "specific-access-approver-admin,case-allocator',N,Y",
+        "9,ABA6,'ctsc-team-leader,ctsc,hmcts-ctsc,specific-access-approver-judiciary,specific-access-approver-ctsc,"
+                + "specific-access-approver-admin,task-supervisor,case-allocator',Y,Y",
 
-        "10,BFA1,'hmcts-ctsc,ctsc',N,N",
-        "10,BFA1,'hmcts-ctsc,ctsc,task-supervisor',Y,N",
-        "10,BFA1,'hmcts-ctsc,ctsc,case-allocator',N,Y",
-        "10,BFA1,'hmcts-ctsc,ctsc,task-supervisor,case-allocator',Y,Y",
-        "10,XYZ1,'',Y,Y"
+        "10,ABA6,'ctsc,hmcts-ctsc',N,N",
+        "10,ABA6,'ctsc,hmcts-ctsc,task-supervisor',Y,N",
+        "10,ABA6,'ctsc,hmcts-ctsc,case-allocator',N,Y",
+        "10,ABA6,'ctsc,hmcts-ctsc,task-supervisor,case-allocator',Y,Y"
     })
     void shouldReturnIacCtscMappings(String roleId, String serviceCode, String expectedRoles,
                                       String taskSupervisorFlag, String caseAllocatorFlag) {
@@ -193,7 +93,7 @@ class DroolProbateStaffOrgRoleMappingTest extends DroolBase {
                 assertEquals(Classification.PRIVATE, r.getClassification());
                 assertEquals(GrantType.BASIC, r.getGrantType());
             } else {
-                assertEquals("IA", r.getAttributes().get("jurisdiction").asText());
+                assertEquals("PROBATE", r.getAttributes().get("jurisdiction").asText());
                 assertEquals(Classification.PUBLIC, r.getClassification());
                 assertEquals(GrantType.STANDARD, r.getGrantType());
             }
@@ -213,18 +113,15 @@ class DroolProbateStaffOrgRoleMappingTest extends DroolBase {
 
     @ParameterizedTest
     @CsvSource({
-        "1,BFA1,'senior-tribunal-caseworker,tribunal-caseworker,hmcts-legal-operations',N,N",
-        "1,BFA1,'senior-tribunal-caseworker,tribunal-caseworker,hmcts-legal-operations,task-supervisor',Y,N",
-        "1,BFA1,'senior-tribunal-caseworker,tribunal-caseworker,hmcts-legal-operations,case-allocator',N,Y",
-        "1,BFA1,'senior-tribunal-caseworker,tribunal-caseworker,hmcts-legal-operations,task-supervisor,"
-                + "case-allocator',Y,Y",
-        "1,XYZ1,'',Y,Y",
+        "1,ABA6,'senior-tribunal-caseworker,tribunal-caseworker,hmcts-legal-operations',N,N",
+        "1,ABA6,'senior-tribunal-caseworker,tribunal-caseworker,hmcts-legal-operations,task-supervisor',Y,N",
+        "1,ABA6,'senior-tribunal-caseworker,tribunal-caseworker,hmcts-legal-operations,case-allocator',N,Y",
+        "1,ABA6,'senior-tribunal-caseworker,tribunal-caseworker,hmcts-legal-operations,task-supervisor,case-allocator',Y,Y",
 
-        "2,BFA1,'tribunal-caseworker,hmcts-legal-operations',N,N",
-        "2,BFA1,'tribunal-caseworker,hmcts-legal-operations,task-supervisor',Y,N",
-        "2,BFA1,'tribunal-caseworker,hmcts-legal-operations,case-allocator',N,Y",
-        "2,BFA1,'tribunal-caseworker,hmcts-legal-operations,task-supervisor,case-allocator',Y,Y",
-        "2,XYZ1,'',Y,Y",
+        "2,ABA6,'tribunal-caseworker,hmcts-legal-operations',N,N",
+        "2,ABA6,'tribunal-caseworker,hmcts-legal-operations,task-supervisor',Y,N",
+        "2,ABA6,'tribunal-caseworker,hmcts-legal-operations,case-allocator',N,Y",
+        "2,ABA6,'tribunal-caseworker,hmcts-legal-operations,task-supervisor,case-allocator',Y,Y"
     })
     void shouldReturnIacLegalOpsMappings(String roleId, String serviceCode, String expectedRoles,
                                      String taskSupervisorFlag, String caseAllocatorFlag) {
@@ -240,7 +137,7 @@ class DroolProbateStaffOrgRoleMappingTest extends DroolBase {
 
         //Execute Kie session
         List<RoleAssignment> roleAssignments =
-                buildExecuteKieSession(getAllFeatureFlagsToggleByJurisdiction("IAC", true));
+                buildExecuteKieSession(getAllFeatureFlagsToggleByJurisdiction("PROBATE", true));
 
         //assertion
         String[] roleNames = StringUtils.isEmpty(expectedRoles) ? new String[0] : expectedRoles.split(",");
@@ -259,7 +156,7 @@ class DroolProbateStaffOrgRoleMappingTest extends DroolBase {
                 assertEquals(Classification.PRIVATE, r.getClassification());
                 assertEquals(GrantType.BASIC, r.getGrantType());
             } else {
-                assertEquals("IA", r.getAttributes().get("jurisdiction").asText());
+                assertEquals("PROBATE", r.getAttributes().get("jurisdiction").asText());
                 assertEquals(Classification.PUBLIC, r.getClassification());
                 assertEquals(GrantType.STANDARD, r.getGrantType());
             }
