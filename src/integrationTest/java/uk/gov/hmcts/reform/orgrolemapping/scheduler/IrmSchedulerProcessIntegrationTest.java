6c3f47a82ac45edf3251554b2025937c9126f052
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-04-09 15:10:50 +0100

    DTSAM-1198 Increase test coverage.

--- src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/scheduler/IrmSchedulerProcessIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/orgrolemapping/scheduler/IrmSchedulerProcessIntegrationTest.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.reform.orgrolemapping.scheduler;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 import uk.gov.hmcts.reform.orgrolemapping.data.irm.IdamRoleManagementQueueEntity;
@@ -12,11 +15,13 @@ import uk.gov.hmcts.reform.orgrolemapping.monitoring.models.ProcessMonitorDto;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static uk.gov.hmcts.reform.orgrolemapping.domain.model.enums.UserType.JUDICIAL;
 
-public class IrmSchedulerProcessIntegrationTest extends BaseIrmSchedulerTestIntegration {
+    public class IrmSchedulerProcessIntegrationTest extends BaseIrmSchedulerTestIntegration {
 
     @Autowired
     private IdamRoleManagementQueueRepository idamRoleManagementQueueRepository;
@@ -25,14 +30,71 @@ public class IrmSchedulerProcessIntegrationTest extends BaseIrmSchedulerTestInte
     private IrmScheduler irmScheduler;
 
     /**
-     * Create queue entry - Success.
+     * Process Judicial Queue - Success.
      */
     @Test
     @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {
         "classpath:sql/irm/queue/init_idam_role_management_queue.sql",
-        "classpath:sql/irm/queue/insert_idam_role_management_queue.sql"
+        "classpath:sql/irm/queue/insert_idam_role_management_queue.sql",
+        "classpath:sql/irm/queue/insert_idam_role_management_queue_caseworker.sql"
     })
     void testProcessJudicialQueue_Success() {
+        testProcessJudicialQueue(2, true, 0);
+    }
+
+    /**
+     * Process Judicial Queue - Retry 1.
+     */
+    @Test
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {
+            "classpath:sql/irm/queue/init_idam_role_management_queue.sql",
+            "classpath:sql/irm/queue/insert_idam_role_management_queue_retry1.sql",
+            "classpath:sql/irm/queue/insert_idam_role_management_queue_caseworker.sql"
+    })
+    void testProcessJudicialQueue_Retry1() {
+        testProcessJudicialQueue(2, true, 0);
+    }
+
+    /**
+     * Process Judicial Queue - Retry 2.
+     */
+    @Test
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {
+            "classpath:sql/irm/queue/init_idam_role_management_queue.sql",
+            "classpath:sql/irm/queue/insert_idam_role_management_queue_retry2.sql",
+            "classpath:sql/irm/queue/insert_idam_role_management_queue_caseworker.sql"
+    })
+    void testProcessJudicialQueue_Retry2() {
+        testProcessJudicialQueue(2, true, 0);
+    }
+
+    /**
+     * Process Judicial Queue - Retry 3.
+     */
+    @Test
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {
+            "classpath:sql/irm/queue/init_idam_role_management_queue.sql",
+            "classpath:sql/irm/queue/insert_idam_role_management_queue_retry3.sql",
+            "classpath:sql/irm/queue/insert_idam_role_management_queue_caseworker.sql"
+    })
+    void testProcessJudicialQueue_Retry3() {
+        testProcessJudicialQueue(2, true, 0);
+    }
+    
+    /**
+     * Process Judicial Queue - Retry 4.
+     */
+    @Test
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {
+            "classpath:sql/irm/queue/init_idam_role_management_queue.sql",
+            "classpath:sql/irm/queue/insert_idam_role_management_queue_retry4.sql",
+            "classpath:sql/irm/queue/insert_idam_role_management_queue_caseworker.sql"
+    })
+    void testProcessJudicialQueue_Retry4() {
+        testProcessJudicialQueue(2, false, 4);
+    }
+
+    void testProcessJudicialQueue(int expectedNoOfRecords, boolean isUpdated, int retry) {
         // GIVEN
 
         // WHEN
@@ -41,19 +103,28 @@ public class IrmSchedulerProcessIntegrationTest extends BaseIrmSchedulerTestInte
         // THEN
         assertNotNull(processMonitorDto);
         assertEquals(EndStatus.SUCCESS, processMonitorDto.getEndStatus());
-        assertNoOfQueueRecords(IdamRecordType.USER, 1);
+        assertQueueRecords(IdamRecordType.USER, expectedNoOfRecords, isUpdated, retry);
     }
 
-    private void assertNoOfQueueRecords(IdamRecordType idamRecordType, int expectedNoOfRecords) {
-        List<IdamRoleManagementQueueEntity> records = idamRoleManagementQueueRepository.findAll();
-        assertEquals(expectedNoOfRecords, records.size());
-        records.forEach(record -> {
-            assertFalse(record.getActive());
-            assertEquals(0, record.getRetry());
-            assertLastUpdatedNow(record.getLastUpdated());
-            assertLastUpdatedNow(record.getLastPublished());
-            assertLastUpdatedNow(record.getRetryAfter());
-            assertEquals(idamRecordType, record.getPublishedAs());
+    private void assertQueueRecords(IdamRecordType idamRecordType, int expectedNoOfRecords,
+                                    boolean isUpdated, int retry) {
+        List<IdamRoleManagementQueueEntity> results = idamRoleManagementQueueRepository.findAll();
+        assertEquals(expectedNoOfRecords, results.size());
+        results.forEach(record -> {
+            if (JUDICIAL.equals(record.getUserType())) {
+                assertEquals(!isUpdated, record.getActive(), "Active mismatch");
+                assertEquals(retry, record.getRetry(), "Retry mismatch");
+                assertEquals(idamRecordType, record.getPublishedAs(), "PublishedAs mismatch");
+                assertTrue(assertLastUpdatedNow(record.getLastUpdated()), "LastUpdated mismatch");
+                if (isUpdated) {
+                    assertTrue(assertLastUpdatedNow(record.getLastPublished()), "LastPublished mismatch");
+                    assertTrue(assertLastUpdatedNow(record.getRetryAfter()), "RetryAfter mismatch");
+                } else {
+                    assertNull(record.getRetryAfter(), "RetryAfter not null");
+                }
+            } else {
+                assertTrue(record.getActive());
+            }
         });
     }
 
