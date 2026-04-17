d9e2c5e6af19e9f02aca810b0f9abc3fb94c7f9b
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-04-17 11:41:57 +0100

    DTSAM-1198 Add the feign interceptor to use the idam credentials.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/feignclients/IdamFeignClient.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/feignclients/IdamFeignClient.java
@@ -12,14 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.irm.IdamInvitation;
 import uk.gov.hmcts.reform.orgrolemapping.domain.model.irm.IdamUser;
 import uk.gov.hmcts.reform.orgrolemapping.feignclients.configuration.FeignClientConfiguration;
-import uk.gov.hmcts.reform.orgrolemapping.feignclients.configuration.FeignClientInterceptor;
 import uk.gov.hmcts.reform.orgrolemapping.feignclients.configuration.IdamFeignClientFallback;
+import uk.gov.hmcts.reform.orgrolemapping.feignclients.configuration.IdamFeignClientInterceptor;
 
 import java.util.List;
 
 
 @FeignClient(value = "idamclient", url = "${feign.client.config.idamClient.url}",
-        configuration = {FeignClientConfiguration.class, FeignClientInterceptor.class},
+        configuration = {FeignClientConfiguration.class, IdamFeignClientInterceptor.class},
         fallback = IdamFeignClientFallback.class)
 
 public interface IdamFeignClient {
