f528b05c44755029bde0ee9c85586aef102310f9
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-04-28 12:52:20 +0100

    DDTSAM-1197 Add an irmQueueController to reset the irm queue.

---
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/controller/testingsupport/IrmQueueController.java
@@ -0,0 +1,117 @@
+package uk.gov.hmcts.reform.orgrolemapping.controller.testingsupport;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static uk.gov.hmcts.reform.orgrolemapping.apihelper.Constants.AUTHORIZATION;
+import static uk.gov.hmcts.reform.orgrolemapping.apihelper.Constants.SERVICE_AUTHORIZATION;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.orgrolemapping.data.irm.IdamRoleManagementQueueEntity;
+import uk.gov.hmcts.reform.orgrolemapping.data.irm.IdamRoleManagementQueueRepository;
+
+@RestController
+@ConditionalOnProperty(name = "testing.support.enabled", havingValue = "true")
+public class IrmQueueController {
+
+    protected static final String FIND_QUEUE_ENTITY
+            = "/am/testing-support/irm/findQueueEntity";
+    protected static final String MAKE_QUEUE_ENTITY_ACTIVE
+            = "/am/testing-support/prm/makeQueueEntityActive";
+
+    private final IdamRoleManagementQueueRepository idamRoleManagementQueueRepository;
+
+    @Autowired
+    public IrmQueueController(IdamRoleManagementQueueRepository idamRoleManagementQueueRepository) {
+        this.idamRoleManagementQueueRepository = idamRoleManagementQueueRepository;
+    }
+
+
+    @GetMapping(
+        path = FIND_QUEUE_ENTITY
+    )
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(
+        summary = "IRM Queue findQueue",
+        security = {
+            @SecurityRequirement(name = AUTHORIZATION),
+            @SecurityRequirement(name = SERVICE_AUTHORIZATION)
+        })
+    @ApiResponse(
+        responseCode = "200",
+        description = "OK",
+        content = @Content(
+            schema = @Schema(implementation = IdamRoleManagementQueueEntity.class),
+            mediaType = APPLICATION_JSON_VALUE
+        )
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "User not found",
+        content = @Content()
+    )
+    public ResponseEntity<IdamRoleManagementQueueEntity> findQueueEntity(
+            @RequestParam() String userId
+    ) {
+        return idamRoleManagementQueueRepository.findById(userId)
+                .map(entity ->
+                        ResponseEntity.ok(entity)
+                )
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+    @PutMapping(
+            path = MAKE_QUEUE_ENTITY_ACTIVE
+    )
+    @ResponseStatus(code = HttpStatus.OK)
+    @Operation(
+        summary = "IRM Queue makeQueueEntryActive",
+        security = {
+            @SecurityRequirement(name = AUTHORIZATION),
+            @SecurityRequirement(name = SERVICE_AUTHORIZATION)
+        })
+    @ApiResponse(
+        responseCode = "200",
+        description = "OK",
+        content = @Content(
+            schema = @Schema(implementation = IdamRoleManagementQueueEntity.class),
+            mediaType = APPLICATION_JSON_VALUE
+        )
+    )
+    @ApiResponse(
+        responseCode = "404",
+        description = "User not found",
+        content = @Content()
+    )
+    public ResponseEntity<IdamRoleManagementQueueEntity> makeQueueEntityActive(
+            @RequestParam() String userId
+    ) {
+        var queueEntityOptional = idamRoleManagementQueueRepository
+                .findById(userId);
+        if (queueEntityOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var queueEntity = queueEntityOptional.get();
+
+        // if not active then activate
+        if (Boolean.FALSE.equals(queueEntity.getActive())) {
+            queueEntity.setActive(true);
+            idamRoleManagementQueueRepository.save(queueEntity);
+        }
+
+        return ResponseEntity.ok(queueEntity);
+    }
+}
