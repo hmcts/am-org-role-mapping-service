6848fa4df6e7217119f9d5d4d47750d7b9850cdc
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-05-11 17:27:19 +0100

    fix build pact test check of version 95cda63b2

--- src/contractTest/java/uk/gov/hmcts/reform/refdata/RefDataJudicialProfileConsumerTestV2.java
+++ src/contractTest/java/uk/gov/hmcts/reform/refdata/RefDataJudicialProfileConsumerTestV2.java
@@ -172,7 +172,7 @@ public class RefDataJudicialProfileConsumerTestV2 {
                         .stringType("jurisdiction")
                         .stringType("ticket_description")
                         .date("start_date", "yyyy-MM-dd")
-                        .minArrayLike("service_codes", 1, (s) -> {
+                        .minArrayLike("service_codes", 0, (s) -> {
                             s.stringType("BFA1");
                         })
                         .stringType("ticket_code")
