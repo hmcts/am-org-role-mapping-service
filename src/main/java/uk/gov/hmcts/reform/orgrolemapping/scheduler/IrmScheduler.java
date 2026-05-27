c8177f4217ba95d7f4e2b8674180152cd8d3b951
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-05-27 11:03:57 +0100

    DTSAM-1200 Remove unused import.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/scheduler/IrmScheduler.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/scheduler/IrmScheduler.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.orgrolemapping.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
