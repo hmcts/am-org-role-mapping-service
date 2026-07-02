4b0685d1483032d07a90c03fbd8fa20adf7896d3
Antony Leons <antony@leons.dev>
2026-07-02 16:00:46 +0100

    Enable FR WA flag for preview

--- src/main/resources/db/migration/V20260526_1208__COT-1208_FR_WA_1_0_base_flag_addition.sql
+++ src/main/resources/db/migration/V20260526_1208__COT-1208_FR_WA_1_0_base_flag_addition.sql
@@ -8,6 +8,10 @@ SELECT 'fr_wa_1_0', 'pr', 'divorce', 'true'
 WHERE NOT EXISTS (SELECT 1 FROM flag_config WHERE flag_name = 'fr_wa_1_0' AND env = 'pr');
 
 INSERT INTO flag_config (flag_name, env, service_name, status)
+SELECT 'fr_wa_1_0', 'preview', 'divorce', 'true'
+WHERE NOT EXISTS (SELECT 1 FROM flag_config WHERE flag_name = 'fr_wa_1_0' AND env = 'preview');
+
+INSERT INTO flag_config (flag_name, env, service_name, status)
 SELECT 'fr_wa_1_0', 'aat', 'divorce', 'true'
 WHERE NOT EXISTS (SELECT 1 FROM flag_config WHERE flag_name = 'fr_wa_1_0' AND env = 'aat');
 
@@ -28,7 +32,7 @@ SELECT 'fr_wa_1_0', 'prod', 'divorce', 'false'
 WHERE NOT EXISTS (SELECT 1 FROM flag_config WHERE flag_name = 'fr_wa_1_0' AND env = 'prod');
 
 UPDATE flag_config SET service_name = 'divorce', status = 'true'
-WHERE flag_name = 'fr_wa_1_0' AND env IN ('local', 'pr', 'aat');
+WHERE flag_name = 'fr_wa_1_0' AND env IN ('local', 'pr', 'preview', 'aat');
 
 UPDATE flag_config SET service_name = 'divorce', status = 'false'
 WHERE flag_name = 'fr_wa_1_0' AND env IN ('demo', 'perftest', 'ithc', 'prod');
