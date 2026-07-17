62f420a05dd6af0c441a72669fe8c00cc8139069
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2026-07-17 10:20:11 +0100

    DTSAM-1278 Add the missing feigncliient settings.

--- src/contractTest/java/uk/gov/hmcts/reform/refdata/RefDataProfessionalUserInternalConsumerTestV2.java
+++ src/contractTest/java/uk/gov/hmcts/reform/refdata/RefDataProfessionalUserInternalConsumerTestV2.java
@@ -40,7 +40,11 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @ContextConfiguration(classes = {RefDataConsumerApplication.class})
 @TestPropertySource(properties = {
     "feign.client.config.crdclient.url=http://localhost:8991",
-    "feign.client.config.prdClient.url=http://localhost:8090"
+    "feign.client.config.prdClient.url=http://localhost:8090",
+    "feign.client.config.jrdClient.url=http://localhost:8091",
+    "feign.client.config.roleAssignmentApp.url=http://localhost:8092",
+    "feign.client.config.ccdClient.url=http://localhost:8093",
+    "feign.client.config.jbsClient.url=http://localhost:8094"
 })
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class RefDataProfessionalUserInternalConsumerTestV2 {
